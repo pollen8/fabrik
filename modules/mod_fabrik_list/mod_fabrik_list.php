917a6c9c9170b722a9a220465776541e735f955b
Rob Clayburn <rob@pollen-8.co.uk>
2011-09-15 14:43:44 +0200

    fixed list module was trying to set view id, which is no longer used

--- modules/mod_fabrik_list/mod_fabrik_list.php
+++ modules/mod_fabrik_list/mod_fabrik_list.php
@@ -10,44 +10,44 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-//load front end language file as well
-$lang = JFactory::getLanguage();
-$lang->load('com_fabrik', JPATH_BASE.DS.'components'.DS.'com_fabrik');
+
 
 if (!defined('COM_FABRIK_FRONTEND')) {
 	JError::raiseError(400, JText::_('COM_FABRIK_SYSTEM_PLUGIN_NOT_ACTIVE'));
 }
-FabrikHelperHTML::framework();
+
 jimport('joomla.filesystem.file');
 jimport('joomla.application.component.model');
 jimport('joomla.application.component.helper');
 JModel::addIncludePath(COM_FABRIK_FRONTEND.DS.'models', 'FabrikFEModel');
-
-$app = JFactory::getApplication();
+JModel::addIncludePath(COM_FABRIK_FRONTEND.DS.'models');
+JTable::addIncludePath(COM_FABRIK_BASE.DS.'administrator'.DS.'components'.DS.'com_fabrik'.DS.'tables');
 
 require_once(COM_FABRIK_FRONTEND.DS.'controller.php');
 require_once(COM_FABRIK_FRONTEND.DS.'controllers'.DS.'list.php');
-
-//$$$rob looks like including the view does something to the layout variable
-$origLayout = JRequest::getVar('layout');
 require_once(COM_FABRIK_FRONTEND.DS.'views'.DS.'list'.DS.'view.html.php');
-JRequest::setVar('layout', $origLayout);
-
 require_once(COM_FABRIK_FRONTEND.DS.'views'.DS.'package'.DS.'view.html.php');
-
-
-JModel::addIncludePath(COM_FABRIK_FRONTEND.DS.'models');
-JTable::addIncludePath(COM_FABRIK_BASE.DS.'administrator'.DS.'components'.DS.'com_fabrik'.DS.'tables');
-$document = JFactory::getDocument();
-
 require_once(COM_FABRIK_FRONTEND.DS.'controllers'.DS.'package.php');
 require_once(COM_FABRIK_FRONTEND.DS.'views'.DS.'form'.DS.'view.html.php');
 
-$listId			= intval($params->get('list_id', 1));
-$useajax			= intval($params->get('useajax', 0));
-$random 			= intval($params->get('radomizerecords', 0));
-$limit				= intval($params->get('limit', 0));
-$layout				=  $params->get('fabriklayout', 'default');
+//load front end language file as well
+$lang = JFactory::getLanguage();
+$lang->load('com_fabrik', JPATH_BASE.DS.'components'.DS.'com_fabrik');
+
+$app = JFactory::getApplication();
+$document = JFactory::getDocument();
+
+FabrikHelperHTML::framework();
+//$$$rob looks like including the view does something to the layout variable
+$origLayout = JRequest::getVar('layout');
+JRequest::setVar('layout', $origLayout);
+
+
+$listId = (int)$params->get('list_id', 1);
+$useajax = (int)$params->get('useajax', 0);
+$random	= (int)$params->get('radomizerecords', 0);
+$limit = (int)$params->get('limit', 0);
+$layout	= $params->get('fabriklayout', 'default');
 
 JRequest::setVar('layout', $layout);
 if ($limit !== 0) {
@@ -55,12 +55,9 @@ if ($limit !== 0) {
 	JRequest::setVar('limit', $limit);
 }
 
-/*this all works fine for a list
- * going to try to load a package so u can access the form and list
- */
-$moduleclass_sfx	= $params->get('moduleclass_sfx', '');
-//if (!$useajax) {
-$listId = intval($params->get( 'list_id', 1 ));
+$moduleclass_sfx = $params->get('moduleclass_sfx', '');
+
+$listId = (int)$params->get('list_id', 1);
 
 $viewName = 'list';
 $viewType	= $document->getType();
@@ -70,7 +67,7 @@ $controller = new FabrikControllerList();
 $view = clone($controller->getView($viewName, $viewType));
 
 // Push a model into the view
-$model	= $controller->getModel($viewName, 'FabrikFEModel');
+$model = $controller->getModel($viewName, 'FabrikFEModel');
 $model->setId($listId);
 $model->randomRecords = $random;
 if (!JError::isError($model)) {
@@ -79,7 +76,6 @@ if (!JError::isError($model)) {
 $view->isMambot = true;
 // Display the view
 $view->assign('error', $controller->getError());
-$view->setId($listId);
 echo $view->display();
 
 JRequest::setVar('layout', $origLayout);
