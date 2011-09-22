c15d295d9440d8d0865b710427a5ec6fecea8d8e
Rob Clayburn <rob@pollen-8.co.uk>
2011-09-22 15:15:43 +0200

    tidying up form view tmpls, if no form error message given revert to standard Jtext validation failed msg

--- components/com_fabrik/views/form/tmpl/mint/default_group.php
+++ components/com_fabrik/views/form/tmpl/mint/default_group.php
@@ -1,17 +1,14 @@
 <ul>
-<?php foreach ( $this->elements as $element) {
+<?php foreach ($this->elements as $element) {
 	?>
 	<li <?php echo @$element->column;?> class="<?php echo $element->containerClass;?>">
-		<?php echo $element->label;?>
-		<?php echo $element->errorTag; ?>
+		<div class="leftCol">
+			<?php echo $element->label;?>
+			<?php echo $element->errorTag; ?>
+		</div>
 		<div class="fabrikElement">
 			<?php echo $element->element;?>
-
 		</div>
-		<div class="fabrikErrorMessage">
-				<?php echo $element->error;?>
-			</div>
-		<div style="clear:both"></div>
 	</li>
 	<?php }?>
 </ul>
