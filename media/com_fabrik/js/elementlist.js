04a4da0a9d2abaa8cc8f15779d8ec67c42f8e33d
Rob Clayburn <rob@pollen-8.co.uk>
2011-10-06 12:16:35 +0200

    more refractoring js into elementlist.js - watchadd

--- media/com_fabrik/js/elementlist.js
+++ media/com_fabrik/js/elementlist.js
@@ -36,5 +36,45 @@ var FbElementList =  new Class({
 				});
 			});
 		}
+	},
+	
+	watchAdd: function () {
+		var val;
+		if (this.options.allowadd === true && this.options.editable !== false) {
+			var id = this.options.element;
+			var c = this.getContainer();
+			c.getElement('input[type=button]').addEvent('click', function (e) {
+				var l = c.getElement('input[name=addPicklistLabel]');
+				var v = c.getElement('input[name=addPicklistValue]');
+				var label = l.value;
+				if (v) {
+					val = v.value;
+				} else {
+					val = label;
+				}
+				if (val === '' || label === '') {
+					alert(Joomla.JText._('PLG_ELEMENT_CHECKBOX_ENTER_VALUE_LABEL'));
+				}
+				else {
+					var r = this.subElements.getLast().findUp('li').clone();
+					r.getElement('input').value = val;
+					var lastid = r.getElement('input').id.replace(id + '_', '').toInt();
+					lastid++;
+					r.getElement('input').checked = 'checked';
+					r.getElement('input').id = id + '_' + lastid;
+					r.getElement('label').setProperty('for', id + '_' + lastid);
+					r.getElement('span').set('text', label);
+					r.inject(this.subElements.getLast().findUp('li'), 'after');
+					this._getSubElements();
+					e.stop();
+					if (v) {
+						v.value = '';
+					}
+					l.value = '';
+					this.addNewOption(val, label);
+					this.mySlider.toggle();
+				}
+			}.bind(this));
+		}
 	}
 });
--- plugins/fabrik_element/radiobutton/radiobutton.js
+++ plugins/fabrik_element/radiobutton/radiobutton.js
@@ -33,43 +33,6 @@ var FbRadio = new Class({
 		}.bind(this));
 	},
 
-	watchAdd: function () {
-		var val;
-		if (this.options.allowadd === true && this.options.editable !== false) {
-			var id = this.options.element;
-			var c = this.getContainer();
-			c.getElement('input[type=button]').addEvent('click', function (event) {
-				var l = c.getElement('input[name=addPicklistLabel]');
-				var v = c.getElement('input[name=addPicklistValue]');
-				var label = l.value;
-				if (v) {
-					val = v.value;
-				} else {
-					val = label;
-				}
-				if (val === '' || label === '') {
-					alert(Joomla.JText._('PLG_ELEMENT_RADIO_ENTER_VALUE_LABEL'));
-				} else {
-					var r = this.subElements.getLast().findUp('div').clone();
-					r.getElement('input').value = val;
-					var lastid = r.getElement('input').id.replace(id + '_', '').toInt();
-					lastid++;
-					r.getElement('input').checked = 'checked';
-					r.getElement('input').id = id + '_' + lastid;
-					r.getElement('span').set('text', label);
-					r.inject(this.subElements.getLast().findUp('div'));
-					this._getSubElements();
-					e.stop();
-					if (v) {
-						v.value = '';
-					}
-					l.value = '';
-					this.addNewOption(val, label);
-				}
-			}.bind(this));
-		}
-	},
-
 	getValue: function () {
 		if (!this.options.editable) {
 			return this.options.value;
