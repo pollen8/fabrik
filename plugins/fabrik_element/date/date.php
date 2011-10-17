7269cbb0b30b1d4620aa7b972cc21f9c57668ecf
Rob Clayburn <rob@pollen-8.co.uk>
2011-10-17 22:01:23 +0200

    fixing date filtering see here - http://fabrikar.com/forums/showthread.php?t=22782

--- plugins/fabrik_element/date/date.php
+++ plugins/fabrik_element/date/date.php
@@ -881,16 +881,17 @@ class plgFabrik_ElementDate extends plgFabrik_Element
 		switch ($filterType) {
 			case 'field':
 			case 'dropdown':
-				$mysql = $this->tableDateToMySQL($value);
-				if ($mysql !== false) {
-					$value = $mysql;
-				}
 
 				if (!$params->get('date_showtime', 0) || $exactTime == false) {
 					//$$$ rob turn into a ranged filter to search the entire day
 					$value = (array)$value;
 					$condition = 'BETWEEN';
 					$value[1] = date("Y-m-d H:i:s", strtotime($this->addDays($value[0], 1)) - 1);
+				} else {
+					$mysql = $this->tableDateToMySQL($value);
+					if ($mysql !== false) {
+						$value = $mysql;
+					}
 				}
 				break;
 
@@ -905,7 +906,8 @@ class plgFabrik_ElementDate extends plgFabrik_Element
 				break;
 		}
 		$this->_resetToGMT = true;
-		return parent::getFilterValue($value, $condition, $eval);
+		$value = parent::getFilterValue($value, $condition, $eval);
+		return $value;
 	}
 
 	/**
