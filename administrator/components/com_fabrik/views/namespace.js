f363eb165d735475f2d52cd6cf543dac5c69e666
Rob Clayburn <rob@pollen-8.co.uk>
2011-10-17 17:03:43 +0200

    Fabrik.addEvent missing from admin juser form plugin not storing data if email not sent (changed so that it does)

--- administrator/components/com_fabrik/views/namespace.js
+++ administrator/components/com_fabrik/views/namespace.js
@@ -1,3 +1,38 @@
-Fabrik = {};
+(function () {
+	Fabrik = {};
 //various Joomla element plugins used to control JForm elements
-Fabrik.model = {'fields': {'fabriktable': {}, 'element': {}}};
+	Fabrik.model = {'fields': {'fabriktable': {}, 'element': {}}};
+	Fabrik.events = {};
+	Fabrik.addEvent = function (type, fn) {
+		if (!Fabrik.events[type]) {
+			Fabrik.events[type] = [];
+		}
+		if (!Fabrik.events[type].contains(fn)) {
+			Fabrik.events[type].push(fn);
+		}
+	};
+
+	Fabrik.addEvents = function (events) {
+		for (var event in events) {
+			Fabrik.addEvent(event, events[event]);
+		}
+		return this;
+	};
+
+	Fabrik.fireEvent = function (type, args, delay) {
+		var events = Fabrik.events;
+		if (!events || !events[type]) {
+			return this;
+		}
+		args = Array.from(args);
+
+		events[type].each(function (fn) {
+			if (delay) {
+				fn.delay(delay, this, args);
+			} else {
+				fn.apply(this, args);
+			}
+		}, this);
+		return this;
+	};
+}());
