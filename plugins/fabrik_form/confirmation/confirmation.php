2a9995b4056e42528ba8f7cf021fae91af028419
Rob Clayburn <rob@pollen-8.co.uk>
2011-10-24 20:26:13 +0200

    confirmation plugin no longre runs on ajax submit, may want to revist this later on, but with it on the form was not saving and the confirmation was not being shown either

--- plugins/fabrik_form/confirmation/confirmation.php
+++ plugins/fabrik_form/confirmation/confirmation.php
@@ -58,7 +58,7 @@ class plgFabrik_FormConfirmation extends plgFabrik_Form {
 
 	function onBeforeStore(&$params, &$formModel)
 	{
-		if (JRequest::getInt('fabrik_ignorevalidation') === 1) {
+		if (JRequest::getInt('fabrik_ignorevalidation') === 1 || JRequest::getInt('fabrik_ajax') === 1) {
 			//saving via inline edit - dont want to confirm
 			return true;
 		}
