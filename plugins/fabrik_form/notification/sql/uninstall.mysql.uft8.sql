669773e9722b9d08f1966f17837ad2f06b3189e1
Rob Clayburn <rob@pollen-8.co.uk>
2011-10-26 12:31:01 +0200

    updating xml files so that createdate and year are inserted by build script

--- plugins/fabrik_form/notification/sql/uninstall.mysql.uft8.sql
+++ plugins/fabrik_form/notification/sql/uninstall.mysql.uft8.sql
@@ -1,6 +1,3 @@
-DELETE FROM `#__fabrik_notification`;
 DROP TABLE `#__fabrik_notification`;
-DELETE FROM `#__fabrik_notification_event`;
 DROP TABLE `#__fabrik_notification_event`;
-DELETE FROM `#__fabrik_notification_event_sent`;
 DROP TABLE `#__fabrik_notification_event_sent`;
