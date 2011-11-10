0c43a4c3b966a75c5d1dfdd3b8a8a0de412fff1b
Rob Clayburn <rob@pollen-8.co.uk>
2011-11-10 11:06:34 +0100

    fixed: isemal validation using deprecated ereg php code, replaced with preg_match

--- plugins/fabrik_validationrule/isemail/isemail.php
+++ plugins/fabrik_validationrule/isemail/isemail.php
@@ -48,7 +48,7 @@ class plgFabrik_ValidationruleIsEmail extends plgFabrik_Validationrule
 			return true;
 		}
 		/* First, we check that there's one symbol, and that the lengths are right*/
-		if (!ereg("[^@]{1,64}@[^@]{1,255}", $email)) {
+		if (!preg_match("/[^@]{1,64}@[^@]{1,255}/", $email)) {
 			/* Email invalid because wrong number of characters in one section, or wrong number of symbols.*/
 			return false;
 		}
@@ -56,20 +56,20 @@ class plgFabrik_ValidationruleIsEmail extends plgFabrik_Validationrule
 		/* Split it into sections to make life easier*/
 		$email_array = explode("@", $email);
 		$local_array = explode(".", $email_array[0]);
-		for ( $i = 0; $i < sizeof($local_array); $i++ ) {
-			if ( !ereg( "^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $local_array[0] ) ) {
+		for ($i = 0; $i < sizeof($local_array); $i++) {
+			if (!preg_match("/^(([A-Za-z0-9!#$%&'*+\/=?^_`{|}~-][A-Za-z0-9!#$%&'*+\/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$/", $local_array[0])) {
 				return false;
 			}
 		}
 		/* Check if domain is IP. If not, it should be valid domain name */
-		if ( !ereg( "^\[?[0-9\.]+\]?$", $email_array[1] ) ) {
+		if (!preg_match("/^\[?[0-9\.]+\]?$/", $email_array[1])) {
 			$domain_array = explode(".", $email_array[1]);
-			if ( sizeof( $domain_array ) < 2 ) {
+			if (sizeof( $domain_array ) < 2) {
 				 /* Not enough parts to domain */
 				return false;
 			}
-			for ( $i = 0; $i < sizeof( $domain_array); $i++ ) {
-				if ( !ereg( "^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i])) {
+			for ($i = 0; $i < sizeof( $domain_array); $i++) {
+				if (!preg_match("/^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$/", $domain_array[$i])) {
 					return false;
 				}
 			}
