baa65552ed2ed773de7049498000028fbe97bf36
Rob Clayburn <rob@pollen-8.co.uk>
2011-12-20 14:53:17 +0100

    removed: podion tmpls

--- components/com_fabrik/views/form/tmpl/podion/default_relateddata.php
+++
@@ -1,9 +0,0 @@
-<?php if (!empty($this->linkedTables)) {?>
-	<ul class='linkedTables'>
-		<?php foreach ($this->linkedTables as $a) { ?>
-		<li>
-			<?php echo implode(" ", $a);?>
-			</li>
-		<?php }?>
-	</ul>
-<?php }?>
