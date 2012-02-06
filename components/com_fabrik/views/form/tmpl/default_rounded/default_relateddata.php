e5063d8bac61aebb58e3485cea93e68fdc7ea1d6
rw1 <web@robinwyllie.com>
2012-02-06 21:24:44 +1100

    adding files

---
+++ components/com_fabrik/views/form/tmpl/default_rounded/default_relateddata.php
@@ -0,0 +1,9 @@
+<?php if (!empty($this->linkedTables)) {?>
+	<ul class='linkedTables'>
+		<?php foreach ($this->linkedTables as $a) { ?>
+		<li>
+			<?php echo implode(" ", $a);?>
+			</li>
+		<?php }?>
+	</ul>
+<?php }?>
