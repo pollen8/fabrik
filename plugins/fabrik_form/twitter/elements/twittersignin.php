049b625096014dca28c8683997e50b56281454f5
pollen8 <rob@pollen-8.co.uk>
2012-03-09 11:39:00 +0100

    fixed: twitter plugin, clear credentials link in the wrong place

--- plugins/fabrik_form/twitter/elements/twittersignin.php
+++ plugins/fabrik_form/twitter/elements/twittersignin.php
@@ -66,8 +66,8 @@ class JFormFieldTwittersignin extends JFormField
 
 		$js = "window.open('$href', 'twitterwins', 'width=800,height=460,toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes');return false;";
 		$str =  '<a href="#" onclick="'.$js.'"><img src="'.COM_FABRIK_LIVESITE.'components/com_fabrik/libs/abraham-twitteroauth/images/lighter.png" alt="Sign in with Twitter"/></a>';
-		$str .= " | <a href=\"#\" onclick=\"$clearjs\">" .    JText::_('PLG_FORM_TWITTER_CLEAR_CREDENTIALS') . "</a><br/>";
-		$str .= "<br /><input type=\"text\" readonly=\"readonly\" name=\"". $this->name . "\" id=\"" .$this->id . "\" value=\"" . $this->value . "\" />";
+		$str .= " | <button class=\"button\" href=\"#\" onclick=\"$clearjs\">" .    JText::_('PLG_FORM_TWITTER_CLEAR_CREDENTIALS') . "</button><br/>";
+		$str .= "<br /><input type=\"hidden\" readonly=\"readonly\" name=\"". $this->name . "\" id=\"" .$this->id . "\" value=\"" . $this->value . "\" />";
 		return $str;
 	}
 }
