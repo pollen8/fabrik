249521cd837f48ff3673ddd201b480a4c8b40c7d
pollen8 <rob@pollen-8.co.uk>
2012-05-07 21:20:02 +0200

    fixed: limit form plugin was not working correctly when using rowid=-1

--- plugins/fabrik_form/limit/limit.php
+++ plugins/fabrik_form/limit/limit.php
@@ -12,20 +12,19 @@
 defined('_JEXEC') or die();
 
 //require the abstract plugin class
-require_once(COM_FABRIK_FRONTEND.DS.'models'.DS.'plugin-form.php');
+require_once(COM_FABRIK_FRONTEND . '/models/plugin-form.php');
 
 class plgFabrik_FormLimit extends plgFabrik_Form {
 
 	/**
 	 * process the plugin, called when form is loaded
-	 * @param object $params
-	 * @param object form model
-	 * @returns bol
+	 * @param	object	$params
+	 * @param	object	form model
+	 * @returns	bool
 	 */
 
 	function onLoad($params, &$formModel)
 	{
-		//FabrikHelperHTML::script('administrator/components/com_fabrik/views/namespace.js');
 		return $this->_process($params, $formModel);
 	}
 
@@ -33,42 +32,52 @@ class plgFabrik_FormLimit extends plgFabrik_Form {
 	{
 		$user = JFactory::getUser();
 		$db = FabrikWorker::getDbo();
-		if ($params->get('limit_allow_anonymous')) {
+		$query = $db->getQuery(true);
+		if ($params->get('limit_allow_anonymous'))
+		{
 			return true;
 		}
-		if (JRequest::getCmd('view') === 'details' || JRequest::getInt('rowid', 0) > 0) {
+		if (JRequest::getCmd('view') === 'details' || $formModel->getRowId() > 0)
+		{
 			return true;
 		}
-
-		$listid = (int)$params->get('limit_table');
-		if ($listid === 0) {
+		$listid = (int) $params->get('limit_table');
+		if ($listid === 0)
+		{
 			//use the limit setting supplied in the admin params
-			$limit = (int)$params->get('limit_length');
-		} else {
+			$limit = (int) $params->get('limit_length');
+		}
+		else
+		{
 			//look up the limit from the table spec'd in the admin params
 			$listModel = JModel::getInstance('List', 'FabrikFEModel');
 			$listModel->setId($listid);
-			$max = $db->NameQuote(FabrikString::shortColName($params->get('limit_max')));
-			$userfield = $db->NameQuote(FabrikString::shortColName($params->get('limit_user')));
-			$db->setQuery("SELECT $max FROM " . $listModel->getTable()->db_table_name . " WHERE $userfield = " . (int)$user->get('id'));
-			$limit = (int)$db->loadResult();
+			$max = $db->quoteName(FabrikString::shortColName($params->get('limit_max')));
+			$userfield = $db->quoteName(FabrikString::shortColName($params->get('limit_user')));
+			$query->select($max)->from($listModel->getTable()->db_table_name)->where($userfield  . ' = ' . (int) $user->get('id'));
+			$db->setQuery($query);
+			$limit = (int) $db->loadResult();
 
 		}
 		$field = $params->get('limit_userfield');
 		$listModel = $formModel->getlistModel();
 		$list = $listModel->getTable();
 		$db = $listModel->getDb();
-		$db->setQuery("SELECT COUNT($field) FROM $list->db_table_name WHERE $field = " . (int)$user->get('id'));
+		$query->clear()->select(' COUNT(' . $field . ')')->from($list->db_table_name)->where($field . ' = ' . (int) $user->get('id'));
+		$db->setQuery($query);
 
 		$c = $db->loadResult();
-		if ($c >= $limit) {
-			$msg = $params->get('limit_reached_message');
+		if ($c >= $limit)
+		{
+			$msg = $params->get('limit_reached_message', JText::sprintf('PLG_FORM_LIMIT_LIMIT_REACHED', $limit));
 			$msg = str_replace('{limit}', $limit, $msg);
 			JError::raiseNotice(1, $msg);
 			return false;
-		} else {
+		}
+		else
+		{
 			$app = JFactory::getApplication();
-			$app->enqueueMessage(JText::sprintf('ENTRIES_LEFT_MESSAGE', $limit - $c, $limit));
+			$app->enqueueMessage(JText::sprintf('PLG_FORM_LIMIT_ENTRIES_LEFT_MESSAGE', $limit - $c, $limit));
 		}
 		return true;
 	}
