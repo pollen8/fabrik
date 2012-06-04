7e8c0306ce3950667dc601b504f1979b4c8bad31
Hugh Messenger <hugh.messenger@gmail.com>
2012-06-03 19:49:52 -0500

    Small fix to email plugin debug.php template

--- plugins/fabrik_form/email/tmpl/debug.php
+++ plugins/fabrik_form/email/tmpl/debug.php
@@ -3,6 +3,9 @@
 <table>
 <?php
 foreach ($this->data as $key => $val) {
+  if ($key === 'join') {
+  	continue;
+  }
   echo "<tr><td>$key</td><td>";
   if (is_array($val)) {
   	foreach ($val as $v) {
