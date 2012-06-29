7b73e16935f301bb57aa2614d210e01bcd118578
trob <beate@.(none)>
2012-06-29 20:37:14 +0200

    filter button mod. because of error if list in module

--- components/com_fabrik/views/list/tmpl/default/default_filter.php
+++ components/com_fabrik/views/list/tmpl/default/default_filter.php
@@ -8,9 +8,8 @@
 	}?>
 		<?php if ($this->filter_action != 'onchange') {?>
 	<li>
-		<button class="fabrik_filter_submit button" value="<?php echo JText::_('COM_FABRIK_GO');?>" name="filter" >
-		<?php echo FabrikHelperHTML::image('search.png', 'list', $this->tmpl);?>
-		</button>
+		<input type="button" class="fabrik_filter_submit button" value="<?php echo JText::_('COM_FABRIK_GO');?>" name="filter" >
+		
 	</li>
 	<?php } ?>	
 	</ul>
