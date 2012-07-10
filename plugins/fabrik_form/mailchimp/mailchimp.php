7316cf85a06901a7002cd3f224151402e97bdfcf
pollen8 <rob@pollen-8.co.uk>
2012-07-10 16:48:02 +0200

    added: mailchimp form plugin: option to automatically sign up to mailchimp added option to pass any custom property to mailchimp

--- plugins/fabrik_form/mailchimp/mailchimp.php
+++ plugins/fabrik_form/mailchimp/mailchimp.php
@@ -1,43 +1,60 @@
 <?php
 /**
- * Add a user to a mailchimp mailing list
- * @package Joomla
- * @subpackage Fabrik
- * @author Rob Clayburn
- * @copyright (C) Rob Clayburn
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @package		Joomla.Plugin
+ * @subpackage	Fabrik.form.mailchimp
+ * @copyright	Copyright (C) 2005 Fabrik. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die();
 
-//require the abstract plugin class
-require_once(COM_FABRIK_FRONTEND . '/models/plugin-form.php');
-require_once('MCAPI.class.php');
+// Require the abstract plugin class
+require_once COM_FABRIK_FRONTEND . '/models/plugin-form.php';
+require_once 'MCAPI.class.php';
 
-class plgFabrik_FormMailchimp extends plgFabrik_Form 
+/**
+ * Add a user to a mailchimp mailing list
+ * 
+ * @package		Joomla.Plugin
+ * @subpackage	Fabrik.form.mailchimp
+ */
+
+class plgFabrik_FormMailchimp extends plgFabrik_Form
 {
 
 	var $html = null;
 
 	/**
-	 * set up the html to be injected into the bottom of the form
+	 * Set up the html to be injected into the bottom of the form
 	 *
-	 * @param	object $params (no repeat counter stuff needed here as the plugin manager
+	 * @param   object  $params (no repeat counter stuff needed here as the plugin manager
 	 * which calls this function has already done the work for you
+	 * @param   object  $formModel  form model
+	 * 
+	 * @return  void
 	 */
 
 	public function getBottomContent($params, $formModel)
 	{
-			$this->html = "
+		if ($params->get('mailchimp_userconfirm', true))
+		{
+		$this->html = "
 			<label class=\"mailchimpsignup\"><input type=\"checkbox\" name=\"fabrik_mailchimp_signup\" class=\"fabrik_mailchimp_signup\" value=\"1\"  />
-			 ".$params->get('mailchimp_signuplabel') . "</label>";
+			 " . $params->get('mailchimp_signuplabel') . "</label>";
+		}
+		else
+		{
+			$this->html = '';
+		}
 	}
 
 	/**
-	 * inject custom html into the bottom of the form
-	 * @param int plugin counter
-	 * @return string html
+	 * Inject custom html into the bottom of the form
+	 * 
+	 * @param   int $c  plugin counter
+	 * 
+	 * @return  string  html
 	 */
 
 	function getBottomContent_result($c)
@@ -46,28 +63,35 @@ class plgFabrik_FormMailchimp extends plgFabrik_Form
 	}
 
 	/**
-	 * process the plugin, called when form is submitted
-	 * @param	object	$params
-	 * @param	object	form
+	 * Process the plugin, called when form is submitted
+	 * 
+	 * @param   object  $params      plugin params
+	 * @param   object  &$formModel  form model
+	 * 
+	 * @return  bool
 	 */
 
 	public function onAfterProcess($params, &$formModel)
 	{
+		
 		$this->formModel = $formModel;
 		$emailData = $this->getEmailData();
 		$post = JRequest::get('post');
-		if (!array_key_exists('fabrik_mailchimp_signup', $post)) {
+		if (!array_key_exists('fabrik_mailchimp_signup', $post) && (bool) $params->get('mailchimp_userconfirm', true) === true)
+		{
 			return;
 		}
 		$listId = $params->get('mailchimp_listid');
 		$apiKey = $params->get('mailchimp_apikey');
-		if ($apiKey == '') {
+		if ($apiKey == '')
+		{
 			JError::raiseNotice(500, 'Mailchimp: no api key specified');
-			return;
+			return false;
 		}
-		if ($listId == '') {
+		if ($listId == '')
+		{
 			JError::raiseNotice(500, 'Mailchimp: no list id specified');
-			return;
+			return false;
 		}
 
 		$api = new MCAPI($params->get('mailchimp_apikey'));
@@ -76,46 +100,63 @@ class plgFabrik_FormMailchimp extends plgFabrik_Form
 
 		$emailKey = $formModel->getElement($params->get('mailchimp_email'), true)->getFullName();
 		$firstNameKey = $formModel->getElement($params->get('mailchimp_firstname'), true)->getFullName();
-		if ($params->get('mailchimp_lastname') !== '') {
+		$fname = $formModel->_formDataWithTableName[$firstNameKey];
+		$opts['FNAME'] = $fname;
+		$opts['NAME'] = $fname;
+		
+		if ($params->get('mailchimp_lastname', '') !== '')
+		{
 			$lastNameKey = $formModel->getElement($params->get('mailchimp_lastname'), true)->getFullName();
 			$lname = $formModel->_formDataWithTableName[$lastNameKey];
 			$opts['LNAME'] = $lname;
+			$opts['NAME'] .= ' ' .$lname;
 		}
 		$email = $formModel->_formDataWithTableName[$emailKey];
-		$fname = $formModel->_formDataWithTableName[$firstNameKey];
-
-		$opts['FNAME'] = $fname;
-
-
+		
+		
 		$w = new FabrikWorker();
 
 		$groupOpts = json_decode($params->get('mailchimp_groupopts', "[]"));
-		if (!empty($groupOpts)) {
-			foreach ($groupOpts as $groupOpt) {
+		if (!empty($groupOpts))
+		{
+			foreach ($groupOpts as $groupOpt)
+			{
 				$groups = array();
-				if (isset($groupOpt->groups)) {
+				if (isset($groupOpt->groups))
+				{
 					$groupOpt->groups = $w->parseMessageForPlaceHolder($groupOpt->groups, $emailData);
-		 			$groups[] = JArrayHelper::fromObject($groupOpt);//array('name'=>'Your Interests:', 'groups'=>'Bananas,Apples')
+					
+					// An arry of additonal options: array('name'=>'Your Interests:', 'groups'=>'Bananas,Apples')
+					$groups[] = JArrayHelper::fromObject($groupOpt);
+				}
+				else
+				{
+					foreach ($groupOpt as $k => $v)
+					{
+						// DOn't use emailData as that contains html markup which is not shown in the list view
+						$opts[strtoupper($k)] = $w->parseMessageForPlaceHolder($v, $formModel->_formData);
+  					}
+					$opts['GROUPINGS'] = $groups;
 				}
 			}
 			$opts['GROUPINGS'] = $groups;
 		}
 
-		// By default this sends a confirmation email - you will not see new members
-		// until the link contained in it is clicked!
-
+		// By default this sends a confirmation email - you will not see new members until the link contained in it is clicked!
 		$emailType = $params->get('mailchimp_email_type', 'html');
-		$doubleOptin = (bool)$params->get('mailchimp_double_optin', true);
-		$updateExisting = (bool)$params->get('mailchimp_update_existing');
+		$doubleOptin = (bool) $params->get('mailchimp_double_optin', true);
+		$updateExisting = (bool) $params->get('mailchimp_update_existing');
 		$retval = $api->listSubscribe($listId, $email, $opts, $emailType, $doubleOptin, $updateExisting);
-		if ($api->errorCode) {
+		if ($api->errorCode)
+		{
 			$formModel->_arErrors['mailchimp_error'] = true;
-			JError::raiseNotice(500, $api->errorCode.':'.$api->errorMessage);
+			JError::raiseNotice(500, $api->errorCode . ':' . $api->errorMessage);
 			return false;
-		} else {
+		}
+		else
+		{
 			return true;
 		}
 
 	}
 }
-?>
