a0e5a9612ed421856e57764daee446947d673dc6
pollen8 <rob@pollen-8.co.uk>
2012-07-19 18:48:26 +0200

    splitting form and field override classes into 2.5 and 3.0 folders (J versions)

--- plugins/system/fabrik/defines.php
+++ plugins/system/fabrik/defines.php
@@ -19,6 +19,7 @@ if (!JFolder::exists('components/com_fabrik/'))
 {
 	return;
 }
+
 define("COM_FABRIK_BASE", str_replace(DIRECTORY_SEPARATOR . 'administrator', '', JPATH_BASE) . DIRECTORY_SEPARATOR);
 define("COM_FABRIK_FRONTEND", COM_FABRIK_BASE . 'components/com_fabrik');
 define("COM_FABRIK_LIVESITE", str_replace('/administrator', '', JURI::base()));
@@ -42,8 +43,8 @@ JHTML::addIncludePath(JPATH_SITE . '/components/com_fabrik/jhelpers/' . $version
 // Register the element class with the loader
 JLoader::register('JElement', JPATH_SITE . '/administrator/components/com_fabrik/element.php');
 
-JLoader::import('components.com_fabrik.classes.formfield', JPATH_SITE . '/administrator', 'administrator.');
-JLoader::import('components.com_fabrik.classes.form', JPATH_SITE . '/administrator', 'administrator.');
+JLoader::import('components.com_fabrik.classes.' . $version->RELEASE . '.formfield', JPATH_SITE . '/administrator', 'administrator.');
+JLoader::import('components.com_fabrik.classes.' . $version->RELEASE . '.form', JPATH_SITE . '/administrator', 'administrator.');
 
 require_once JPATH_ADMINISTRATOR . '/components/com_fabrik/tables/fabtable.php';
 require_once COM_FABRIK_FRONTEND . '/models/fabrik.php';
