422c6f4b13de96cd31d8a5b69d7fc6c35c84313f
pollen8 <rob@pollen-8.co.uk>
2012-07-20 12:35:41 +0200

    fixed: fileupload as ajax uploaded in ajax loaded popup form . On 2nd load of said form js error stopped form from submitting.

--- plugins/fabrik_element/fileupload/fileupload.js
+++ plugins/fabrik_element/fileupload/fileupload.js
@@ -289,7 +289,11 @@ var FbFileUpload = new Class({
 				key = key.split('\\').getLast();
 				var f = document.getElements('input[name*=' + key + ']');
 				f = f[1];
-				f.value = JSON.encode(image);
+				// $$$ rob - seems reloading ajax fileupload element in ajax form (e.g. from db join add record)
+				// is producing odd effects where old fileupload object constains info to previously uploaded image?
+				if (typeOf(f) !== 'null') {
+					f.value = JSON.encode(image);
+				}
 			});
 		}
 		return true;
