20fd8c08afa8792b1720f553b1cc0dbc6a8f35fc
pollen8 <rob@pollen-8.co.uk>
2012-08-15 17:56:46 +0200

    removed: comment form plugin jskit option - no longer available as a service. changed: js code now uses mootools spinner, rather than injected img for waiting when ajax loading

--- plugins/fabrik_form/comment/comments.js
+++ plugins/fabrik_form/comment/comments.js
@@ -19,7 +19,7 @@ var FabrikComment = new Class({
 		this.setOptions(this.getOptions(), options);
 		this.fx = {};
 		this.fx.toggleForms = $H();
-		this.spinner = new Element('img', {'styles': {'display': 'none'},	'src': Fabrik.liveSite + 'media/com_fabrik/images/ajax-loader.gif'});
+		this.spinner = new Spinner('fabrik-comments', {'message': 'loading'});
 		this.doAjaxDeleteComplete = this.deleteComplete.bindWithEvent(this);
 		this.ajax = {};
 		this.ajax.deleteComment = new Request({
@@ -74,10 +74,8 @@ var FabrikComment = new Class({
 		if (typeOf(d.message) !== 'null') {
 			alert(d.message.title, d.message.message);
 		}
-		//for update
-		if (this.spinner) {
-			this.spinner.setStyle('display', 'none');
-		}
+		// For update
+		this.spinner.hide();
 		this.watchInput();
 		this.updateDigg();
 	},
@@ -87,10 +85,9 @@ var FabrikComment = new Class({
 	// ***************************//
 
 	watchInput : function () {
-		var url = Fabrik.liveSite + 'index.php';
 
 		this.ajax.addComment = new Request({
-			'url': url, 
+			'url': 'index.php', 
 			'method': 'get',
 			'data': {
 				'option': 'com_fabrik',
@@ -116,7 +113,6 @@ var FabrikComment = new Class({
 			f.getElement('input[type=button]').addEvent('click', this.doInput.bindWithEvent(this));
 			input.addEvent('click', this.testInput.bindWithEvent(this));
 
-			(this.spinner).inject(this.element.getElement('input[type=button]'), 'after');
 		}.bind(this));
 	},
 
@@ -135,7 +131,7 @@ var FabrikComment = new Class({
 
 	// check details and then submit the form
 	doInput : function (e) {
-		this.spinner.inject(e.target, 'after');
+		this.spinner.show();
 		var replyform = e.target.getParent('.replyform');
 		if (replyform.id === 'master-comment-form') {
 			var lis = this.element.getElement('ul').getElements('li');
@@ -150,14 +146,13 @@ var FabrikComment = new Class({
 
 		if (e.type === 'keydown') {
 			if (e.keyCode.toInt() !== 13) {
-				this.spinner.setStyle('display', 'none');
+				this.spinner.hide();
 				return;
 			}
 		}
-		this.spinner.setStyle('display', '');
 		var v = replyform.getElement('textarea').get('value');
 		if (v === '') {
-			this.spinner.setStyle('display', 'none');
+			this.spinner.hide();
 			alert(Joomla.JText._('PLG_FORM_COMMENT_PLEASE_ENTER_A_COMMENT_BEFORE_POSTING'));
 			return;
 		}
@@ -166,7 +161,7 @@ var FabrikComment = new Class({
 		if (name) {
 			var namestr = name.get('value');
 			if (namestr === '') {
-				this.spinner.setStyle('display', 'none');
+				this.spinner.hide();
 				alert(Joomla.JText._('PLG_FORM_COMMENT_PLEASE_ENTER_A_NAME_BEFORE_POSTING'));
 				return;
 			}
@@ -181,7 +176,7 @@ var FabrikComment = new Class({
 		if (email) {
 			var emailstr = email.get('value');
 			if (emailstr === '') {
-				this.spinner.setStyle('display', 'none');
+				this.spinner.hide();
 				alert(Joomla.JText._('PLG_FORM_COMMENT_ENTER_EMAIL_BEFORE_POSTNG'));
 				return;
 			}
@@ -224,6 +219,7 @@ var FabrikComment = new Class({
 
 	// toggle fx the reply forms - recalled each time a comment is added via ajax
 	watchReply : function () {
+		this.spinner.resize();
 		this.element.getElements('.replybutton').each(function (a) {
 			var fx;
 			a.removeEvents();
