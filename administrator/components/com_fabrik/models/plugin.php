37b983f4c43864884b7308580e274fd5335f4fb6
pollen8 <rob@pollen-8.co.uk>
2012-09-04 11:50:18 +0200

    fixed: list plugins published state always set back to published on save

--- administrator/components/com_fabrik/models/plugin.php
+++ administrator/components/com_fabrik/models/plugin.php
@@ -65,20 +65,18 @@ class FabrikModelPlugin extends JModel
 		}
 		$data = (array) json_decode($item->params);
 		$data['plugin'] = $this->getState('plugin');
-
-		$state = (array) JArrayHelper::getValue($data, 'plugin_state');
-
 		$data['params'] = (array) JArrayHelper::getValue($data, 'params', array());
-		$data['params']['plugin_state'] = JArrayHelper::getValue($state, 0, 1);
 		$data['params']['plugins'] = $this->getState('plugin');
 
 		$data['validationrule']['plugin'] = $this->getState('plugin');
 
 		$c = $this->getState('c') + 1;
 
-		// Add plugin locations and events
+		// Add plugin published state, locations and events
+		$state = (array) JArrayHelper::getValue($data, 'plugin_state');
 		$locations = (array) JArrayHelper::getValue($data, 'plugin_locations');
 		$events = (array) JArrayHelper::getValue($data, 'plugin_events');
+		$data['params']['plugin_state'] = JArrayHelper::getValue($state, $c, 1);
 		$data['plugin_locations'] = JArrayHelper::getValue($locations, $c);
 		$data['plugin_events'] = JArrayHelper::getValue($events, $c);
 
