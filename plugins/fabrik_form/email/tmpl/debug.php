c4d131cebe068d5fca462acf176bc6bfa2f3cdab
pollen8 <rob@pollen-8.co.uk>
2012-09-17 13:54:39 +0200

    tidied up form email plugin tmpl

--- plugins/fabrik_form/email/tmpl/debug.php
+++ plugins/fabrik_form/email/tmpl/debug.php
@@ -1,24 +1,33 @@
-<!-- This is a sample email template. It will just print out all of the request data:
- -->
+<?php
+/**
+ * This is a sample email template. It will just print out all of the request data:
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Fabrik.form.email
+ * @copyright   Copyright (C) 2005 Fabrik. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+?>
 <table>
 <?php
-foreach ($this->data as $key => $val) {
-  if ($key === 'join') {
-  	continue;
-  }
-  echo "<tr><td>$key</td><td>";
-  if (is_array($val)) {
-  	foreach ($val as $v) {
-  		if (is_array($v)) {
-  			echo implode("<br>", $v);
-	  	}else{
-	  		echo implode("<br>", $val);
-	  	}
-  	}
-  } else {
-  	echo $val;
-  }
-  echo "</td></tr>";
+foreach ($this->data as $key => $val)
+{
+	if ($key === 'join') :
+		continue;
+	endif;
+	echo '<tr><td>' . $key . '</td><td>';
+	if (is_array($val)) :
+		foreach ($val as $v):
+			if (is_array($v)) :
+				echo implode("<br>", $v);
+			else;
+				echo implode("<br>", $val);
+			endif;
+		endforeach;
+	else;
+		echo $val;
+	endif;
+	echo "</td></tr>";
 }
 ?>
 </table>
@@ -28,15 +37,15 @@ foreach ($this->data as $key => $val) {
 <table>
 <?php
 $joindata = $this->data['join'];
-foreach (array_keys($joindata) as $joinkey) {
+foreach (array_keys($joindata) as $joinkey) :
 	$keys = array_keys($joindata[$joinkey]);
 	$length = count($joindata[$joinkey][$keys[0]]);
-	for($i = 0; $i < $length; $i++) {
-		echo "<tr><td colspan=\"2\"><h3>record $i</h3></td></tr>";
-		foreach($keys as $k) {
-			echo  "<tr><td>$k</td><td>". $this->data['join'][$joinkey][$k][$i]. "</td></tr>";
-		}
-	}
-}
+	for ($i = 0; $i < $length; $i++) :
+		echo '<tr><td colspan="2"><h3>record ' . $i . '</h3></td></tr>';
+		foreach ($keys as $k) :
+			echo '<tr><td>' . $k . '</td><td>' . $this->data['join'][$joinkey][$k][$i] . '</td></tr>';
+		endforeach;
+	endfor;
+endforeach;
 ?>
 </table>
