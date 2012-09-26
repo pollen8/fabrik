325b6d4139a0cbddb14b7ed53b7c3909a998bf9c
pollen8 <rob@pollen-8.co.uk>
2012-09-26 15:51:50 +0200

    Closes GH-260

--- administrator/components/com_fabrik/models/element.php
+++ administrator/components/com_fabrik/models/element.php
@@ -163,8 +163,7 @@ class FabrikModelElement extends JModelAdmin
 				foreach ($elementModels as $elementModel)
 				{
 					$o = new stdClass;
-					$element = $elementModel->getElement();
-					$o->label = FabrikString::getShortDdLabel($element->label);
+					$o->label = $elementModel->getFullName(false, false, false);
 					$o->value = 'fabrik_trigger_element_' . $elementModel->getFullName(false, true, false);
 					$aEls[] = $o;
 				}
