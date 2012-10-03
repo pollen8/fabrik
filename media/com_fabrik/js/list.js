2ee64c0771c7983776a7ee21d02dbfbe04241e1d
pollen8 <rob@pollen-8.co.uk>
2012-10-03 11:58:22 +0200

    fixed: previous fix for ajax row tmpl not right'

--- media/com_fabrik/js/list.js
+++ media/com_fabrik/js/list.js
@@ -828,14 +828,17 @@ var FbList = new Class({
 				var tbody = this.options.isGrouped ? this.list.getElements('.fabrik_groupdata')[gcounter] : this.tbody;
 				gcounter++;
 				for (i = 0; i < groupData.length; i++) {
-					if (typeOf(this.options.rowtemplate) === 'string') {
-						this.options.rowtemplate = new Element('div').set('html', this.options.rowtemplate).getChildren()[0];
-					}
-					container = this.options.rowtemplate.get('tag') === 'tr' ? 'table' : 'div';
-					thisrowtemplate = new Element(container);
 
-					// ie tmp fix for mt 1.2 setHTML on table issue
-					thisrowtemplate.adopt(this.options.rowtemplate.clone());
+					if (typeOf(this.options.rowtemplate) === 'string') {
+						container = (this.options.rowtemplate.trim().slice(0, 3) === '<tr') ? 'table' : 'div';
+						thisrowtemplate = new Element(container);
+						thisrowtemplate.set('html', this.options.rowtemplate);
+					} else {
+						container = this.options.rowtemplate.get('tag') === 'tr' ? 'table' : 'div';
+						thisrowtemplate = new Element(container);
+						// ie tmp fix for mt 1.2 setHTML on table issue
+						thisrowtemplate.adopt(this.options.rowtemplate.clone());
+					}
 					var row = $H(groupData[i]);
 					$H(row.data).each(function (val, key) {
 						var rowk = '.' + key;
