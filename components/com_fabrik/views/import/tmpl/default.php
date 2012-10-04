9ca7dde636110fdf5a7ac5d4846d0a1618f0aeb3
pollen8 <rob@pollen-8.co.uk>
2012-10-04 12:46:04 +0200

    changed: tidied up import csv view and tmpl

--- components/com_fabrik/views/import/tmpl/default.php
+++ components/com_fabrik/views/import/tmpl/default.php
@@ -1,52 +1,33 @@
 <?php
-
 /**
-* @package Joomla
-* @subpackage Fabrik
-* @copyright Copyright (C) 2005 Rob Clayburn. All rights reserved.
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-*/
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @copyright   Copyright (C) 2005 Fabrik. All rights reserved.
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ */
 
-// no direct access
+// No direct access
 defined('_JEXEC') or die('Restricted access');
 $url = JRoute::_('index.php');
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
-//@TODO load this from the JForm that we use in admin
+$action = JRoute::_('index.php?option=com_fabrik');
 ?>
-<form enctype="multipart/form-data" action="<?php JRoute::_('index.php?option=com_fabrik'); ?>" method="post" name="adminForm" id="fabrik-form" class="form-validate">
+<form enctype="multipart/form-data" action="<?php echo $acton ?>" method="post" name="adminForm" id="fabrik-form" class="form-validate">
 <div class="width-100 fltlft">
-	<?php
-	$id	= JRequest::getInt('listid', 0); // from list data view in admin
-	$cid = JRequest::getVar('cid', array(0));// from list of lists checkbox selection
-	JArrayHelper::toInteger($cid);
-	if ($id === 0) {
-		$id = $cid[0];
-	}
-	if (($id !== 0)) {
-		$db = FabrikWorker::getDbo();
-		$query = $db->getQuery(true);
-		$query->select('label')->from('#__{package}_lists')->where('id = '.$id);
-		$db->setQuery($query);
-		$list = $db->loadResult();
-		print_r($list);
-	}
-	$fieldsets = array('details');
-	$fieldsets[] = $id === 0 ? 'creation' : 'append';
-	$fieldsets[] = 'format';
-	?>
-		<input type="hidden" name="listid" value="<?php echo $id ;?>" />
-<?php foreach ($fieldsets as $fieldset) {?>
+	<input type="hidden" name="listid" value="<?php echo $id; ?>" />
+	<h2><?php echo JText::sprintf('COM_FABRIK_CSV_IMPORT_HEADING', $this->listName); ?></h2>
+	<?php foreach ($this->fieldsets as $fieldset) :?>
 	<fieldset class="adminform">
 		<ul>
 		<?php foreach ($this->form->getFieldset($fieldset) as $field) :?>
 			<li>
-				<?php echo $field->label; ?><?php echo $field->input; ?>
+				<?php echo $field->label . $field->input; ?>
 			</li>
 			<?php endforeach; ?>
 		</ul>
 	</fieldset>
-	<?php }?>
+	<?php endforeach;?>
 
 	<input type="hidden" name="task" value="import.doimport" />
   	<?php echo JHTML::_('form.token');
--- components/com_fabrik/views/import/view.html.php
+++ components/com_fabrik/views/import/view.html.php
@@ -1,20 +1,36 @@
 <?php
 /**
-* @package Joomla
-* @subpackage Fabrik
-* @copyright Copyright (C) 2005 Rob Clayburn. All rights reserved.
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-*/
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @copyright   Copyright (C) 2005 Fabrik. All rights reserved.
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ */
 
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die();
 
 jimport('joomla.application.component.view');
 
-class fabrikViewImport extends JView
+/**
+ * View class for CSV import
+ *
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @since       3.0
+ */
+
+class FabrikViewImport extends JView
 {
 
-	function display($tpl = null)
+	/**
+	 * Display the view
+	 *
+	 * @param   string  $tpl  template
+	 *
+	 * @return  this
+	 */
+
+	public function display($tpl = null)
 	{
 		FabrikHelperHTML::framework();
 		$this->listid = JRequest::getVar('listid', 0);
@@ -22,11 +38,47 @@ class fabrikViewImport extends JView
 		$listModel->setId($this->listid);
 		$this->table = $listModel->getTable();
 		$this->form = $this->get('Form');
-		if (!$listModel->canCSVImport()) {
+		if (!$listModel->canCSVImport())
+		{
 			JError::raiseError(400, 'Naughty naughty!');
-			jexit;
+			jexit();
 		}
+		$this->fieldsets = $this->setFieldSets();
 		parent::display($tpl);
+		return $this;
+	}
+
+	/**
+	 * Set which fieldsets should be used
+	 *
+	 * @since   3.0.7
+	 *
+	 * @return  array  fieldset names
+	 */
+
+	private function setFieldSets()
+	{
+		// From list data view in admin
+		$id = JRequest::getInt('listid', 0);
+
+		// From list of lists checkbox selection
+		$cid = JRequest::getVar('cid', array(0));
+		JArrayHelper::toInteger($cid);
+		if ($id === 0)
+		{
+			$id = $cid[0];
+		}
+		if (($id !== 0))
+		{
+			$db = FabrikWorker::getDbo();
+			$query = $db->getQuery(true);
+			$query->select('label')->from('#__{package}_lists')->where('id = ' . $id);
+			$db->setQuery($query);
+			$this->listName = $db->loadResult();
+		}
+		$fieldsets = array('details');
+		$fieldsets[] = $id === 0 ? 'creation' : 'append';
+		$fieldsets[] = 'format';
+		return $fieldsets;
 	}
 }
-?>
