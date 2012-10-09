c0a5e66d5b3ee72d4e383bc9be9d7ec253605b6b
pollen8 <rob@pollen-8.co.uk>
2012-10-09 16:29:02 +0200

    fixed: incorrect _getStep used rather than getStep

--- components/com_fabrik/views/list/view.csv.php
+++ components/com_fabrik/views/list/view.csv.php
@@ -33,7 +33,7 @@ class FabrikViewList extends JViewLegacy
 		$model->setOutPutFormat('csv');
 		$exporter->model = $model;
 		$input->set('limitstart' . $model->getId(), $input->getInt('start', 0));
-		$input->set('limit' . $model->getId(), $exporter->_getStep());
+		$input->set('limit' . $model->getId(), $exporter->getStep());
 
 		// $$$ rob moved here from csvimport::getHeadings as we need to do this before we get
 		// the list total
