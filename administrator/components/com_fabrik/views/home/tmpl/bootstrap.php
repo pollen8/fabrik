eec674de3c1e9a681f9f3fe3b0940565dadd1f11
pollen8 <rob@pollen-8.co.uk>
2012-10-16 13:47:25 +0200

    fixed: tab in admin homepage

--- administrator/components/com_fabrik/views/home/tmpl/bootstrap.php
+++ administrator/components/com_fabrik/views/home/tmpl/bootstrap.php
@@ -38,86 +38,87 @@ JToolBarHelper::title(JText::_('COM_FABRIK_WELCOME'), 'fabrik.png');
 	<div class="span6">
 		<ul class="nav nav-tabs">
 			<li class="active">
-		    	<a data-toggle="tab" href="#tab-about">
+		    	<a data-toggle="tab" href="#home-about">
 		    		<?php echo JText::_('COM_FABRIK_HOME_ABOUT'); ?>
 		    	</a>
 		    </li>
 		    <li>
-		    	<a data-toggle="tab" href="#tab-news">
+		    	<a data-toggle="tab" href="#home-news">
 		    		<?php echo JText::_('COM_FABRIK_HOME_NEWS'); ?>
 		    	</a>
 		    </li>
 		    <li>
-		    	<a data-toggle="tab" href="#tab-stats">
+		    	<a data-toggle="tab" href="#home-stats">
 		    		<?php echo JText::_('COM_FABRIK_HOME_STATS')?>
 		    	</a>
 		    </li>
 		    <li>
-		    	<a data-toggle="tab" href="#tab-links">
+		    	<a data-toggle="tab" href="#home-links">
 		    		<?php echo JText::_('COM_FABRIK_HOME_USEFUL_LINKS')?>
 		    	</a>
 		    </li>
 		    <li>
-		    	<a data-toggle="tab" href="#tab-tools">
+		    	<a data-toggle="tab" href="#home-tools">
 		    		<?php echo JText::_('COM_FABRIK_HOME_TOOLS')?>
 		    	</a>
 		    </li>
 		</ul>
 
-		<div class="tab-pane active" id="tab-about">
-			<?php echo JText::_('COM_FABRIK_HOME_ABOUT_TEXT'); ?>
-		</div>
+		<div class="tab-content">
+			<div class="tab-pane active" id="home-about">
+				<?php echo JText::_('COM_FABRIK_HOME_ABOUT_TEXT'); ?>
+			</div>
 
-		<div class="tab-pane" id="tab-news">
-			<?php echo $this->feed;?>
-		</div>
+			<div class="tab-pane" id="home-news">
+				<?php echo $this->feed;?>
+			</div>
 
-		<div class="tab-pane" id="tab-stats">
-			<table class='adminlist'>
-			<thead>
-				<tr>
-					<th style="width:20%"><?php echo JText::_('COM_FABRIK_HOME_DATE')?></th>
-					<th><?php echo JText::_('COM_FABRIK_HOME_ACTION')?></th>
-				</tr>
-			</thead>
-			<tbody>
-					<?php foreach ($this->logs as $log) :?>
+			<div class="tab-pane" id="home-stats">
+				<table class='adminlist'>
+				<thead>
 					<tr>
-						<td>
-						<?php echo $log->timedate_created;?>
-						</td>
-						<td>
-						<span class="editlinktip hasTip" title="<?php echo $log->message_type . "::" . $log->message; ?>">
-							<?php echo $log->message_type;?>
-						</span>
-						</td>
+						<th style="width:20%"><?php echo JText::_('COM_FABRIK_HOME_DATE')?></th>
+						<th><?php echo JText::_('COM_FABRIK_HOME_ACTION')?></th>
 					</tr>
-					<?php endforeach;?>
-				</tbody>
-			</table>
-		</div>
+				</thead>
+				<tbody>
+						<?php foreach ($this->logs as $log) :?>
+						<tr>
+							<td>
+							<?php echo $log->timedate_created;?>
+							</td>
+							<td>
+							<span class="editlinktip hasTip" title="<?php echo $log->message_type . "::" . $log->message; ?>">
+								<?php echo $log->message_type;?>
+							</span>
+							</td>
+						</tr>
+						<?php endforeach;?>
+					</tbody>
+				</table>
+			</div>
 
-		<div class="tab-pane" id="tab-links">
-			<ul class="adminlist">
-				<li><a href="http://fabrikar.com/"><?php echo JText::_('COM_FABRIK_HOME_FABRIK_WEB_SITE')?></a></li>
-				<li><a href="http://fabrikar.com/forums"><?php echo JText::_('COM_FABRIK_HOME_FORUM')?></a>
-				<li><a href="http://fabrikar.com/wiki/"><?php echo JText::_('COM_FABRIK_HOME_DOCUMENTATION_WIKI')?></a></li>
-			</ul>
-		</div>
+			<div class="tab-pane" id="home-links">
+				<ul class="adminlist">
+					<li><a href="http://fabrikar.com/"><?php echo JText::_('COM_FABRIK_HOME_FABRIK_WEB_SITE')?></a></li>
+					<li><a href="http://fabrikar.com/forums"><?php echo JText::_('COM_FABRIK_HOME_FORUM')?></a>
+					<li><a href="http://fabrikar.com/wiki/"><?php echo JText::_('COM_FABRIK_HOME_DOCUMENTATION_WIKI')?></a></li>
+				</ul>
+			</div>
 
-		<div class="tab-pane" id="tab-tools">
-			<ul class="adminlist">
-				<li><a href="index.php?option=com_fabrik&task=home.installSampleData">
-				<?php echo JText::_('COM_FABRIK_HOME_INSTALL_SAMPLE_DATA')?></a>
-				</li>
-				<li><a onclick="return confirm('<?php echo JText::_('COM_FABRIK_HOME_CONFIRM_WIPE', true);?>')" href="index.php?option=com_fabrik&task=home.reset">
-					<?php echo JText::_('COM_FABRIK_HOME_RESET_FABRIK') ?>
-				</a></li>
-				<li>
-					<a href="index.php?option=com_fabrik&task=upgrade.check">Upgrade from 2.1</a>
-				</li>
-			</ul>
+			<div class="tab-pane" id="home-tools">
+				<ul class="adminlist">
+					<li><a href="index.php?option=com_fabrik&task=home.installSampleData">
+					<?php echo JText::_('COM_FABRIK_HOME_INSTALL_SAMPLE_DATA')?></a>
+					</li>
+					<li><a onclick="return confirm('<?php echo JText::_('COM_FABRIK_HOME_CONFIRM_WIPE', true);?>')" href="index.php?option=com_fabrik&task=home.reset">
+						<?php echo JText::_('COM_FABRIK_HOME_RESET_FABRIK') ?>
+					</a></li>
+					<li>
+						<a href="index.php?option=com_fabrik&task=upgrade.check">Upgrade from 2.1</a>
+					</li>
+				</ul>
+			</div>
 		</div>
-
 	</div>
 </div>
