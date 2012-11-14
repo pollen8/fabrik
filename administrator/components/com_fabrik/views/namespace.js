a6ed8f2c81705a92072ff3059d54631b6269f0a6
pollen8 <rob@pollen-8.co.uk>
2012-11-14 10:55:03 +0100

    fixed: radio buttons added via ajax were not clickable

--- administrator/components/com_fabrik/views/namespace.js
+++ administrator/components/com_fabrik/views/namespace.js
@@ -3,6 +3,25 @@
 		return;
 	}
 	FabrikAdmin = {};
-	//various Joomla element plugins used to control JForm elements
+	// Various Joomla element plugins used to control JForm elements
 	FabrikAdmin.model = {'fields': {'fabriktable': {}, 'element': {}}};
 }());
+
+// Relay radio button group clicks for content added via ajax calls
+(function ($) {
+	$(document).on('click', '.btn-group label:not(.active)', null, function (event) {
+		var label = $(this);
+		var input = $('#' + label.attr('for'));
+			if (!input.prop('checked')) {
+				label.closest('.btn-group').find("label").removeClass('active btn-success btn-danger btn-primary');
+				if (input.val() == '') {
+					label.addClass('active btn-primary');
+				} else if (input.val() == 0) {
+					label.addClass('active btn-danger');
+				} else {
+					label.addClass('active btn-success');
+				}
+				input.prop('checked', true);
+			}
+	});
+})(jQuery);
