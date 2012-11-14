978855e733d755f7033504b9cc382a40bffec941
pollen8 <rob@pollen-8.co.uk>
2012-11-14 11:10:43 +0100

    mend
    lint.compress js files

--- administrator/components/com_fabrik/views/namespace.js
+++ administrator/components/com_fabrik/views/namespace.js
@@ -12,16 +12,16 @@
 	$(document).on('click', '.btn-group label:not(.active)', null, function (event) {
 		var label = $(this);
 		var input = $('#' + label.attr('for'));
-			if (!input.prop('checked')) {
-				label.closest('.btn-group').find("label").removeClass('active btn-success btn-danger btn-primary');
-				if (input.val() == '') {
-					label.addClass('active btn-primary');
-				} else if (input.val() == 0) {
-					label.addClass('active btn-danger');
-				} else {
-					label.addClass('active btn-success');
-				}
-				input.prop('checked', true);
+		if (!input.prop('checked')) {
+			label.closest('.btn-group').find("label").removeClass('active btn-success btn-danger btn-primary');
+			if (input.val() === '') {
+				label.addClass('active btn-primary');
+			} else if (input.val() === 0) {
+				label.addClass('active btn-danger');
+			} else {
+				label.addClass('active btn-success');
 			}
+			input.prop('checked', true);
+		}
 	});
 })(jQuery);
