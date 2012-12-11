9134b431774a3b54f647c7d57921dfca77afe258
pollen8 <rob@pollen-8.co.uk>
2012-12-11 10:31:33 +0100

    Closes GH-551 - cant empty trash

--- administrator/components/com_fabrik/views/crons/view.html.php
+++ administrator/components/com_fabrik/views/crons/view.html.php
@@ -1,10 +1,10 @@
 <?php
 /**
-* @package Joomla
-* @subpackage Fabrik
-* @copyright Copyright (C) 2005 Rob Clayburn. All rights reserved.
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-*/
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @copyright   Copyright (C) 2005 Fabrik. All rights reserved.
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ */
 
 // No direct access
 defined('_JEXEC') or die;
@@ -14,35 +14,60 @@ jimport('joomla.application.component.view');
 /**
  * View class for a list of crons.
  *
- * @package		Joomla.Administrator
- * @subpackage	Fabrik
- * @since		1.6
+ * @package     Joomla.Administrator
+ * @subpackage  Fabrik
+ * @since       1.6
  */
 class FabrikViewCrons extends JView
 {
+	/**
+	 * Cron jobs
+	 * @var  array
+	 */
 	protected $items;
+
+	/**
+	 * Pagination
+	 * @var  JPagination
+	 */
 	protected $pagination;
+
+	/**
+	 * View state
+	 * @var  object
+	 */
 	protected $state;
 
 	/**
 	 * Display the view
+	 *
+	 * @param   string  $tpl  Template
+	 *
+	 * @return  void
 	 */
+
 	public function display($tpl = null)
 	{
 		// Initialise variables.
-		$this->categories	= $this->get('CategoryOrders');
-		$this->items		= $this->get('Items');
-		$this->pagination	= $this->get('Pagination');
-		$this->state		= $this->get('State');
+		$this->categories = $this->get('CategoryOrders');
+		$this->items = $this->get('Items');
+		$this->pagination = $this->get('Pagination');
+		$this->state = $this->get('State');
 		$this->packageOptions = $this->get('PackageOptions');
 
 		// Check for errors.
-		if (count($errors = $this->get('Errors'))) {
+		if (count($errors = $this->get('Errors')))
+		{
 			JError::raiseError(500, implode("\n", $errors));
 			return false;
 		}
-
+		FabrikAdminHelper::setViewLayout($this);
 		$this->addToolbar();
+		FabrikAdminHelper::addSubmenu(JRequest::getWord('view', 'lists'));
+		if (FabrikWorker::j3())
+		{
+			$this->sidebar = JHtmlSidebar::render();
+		}
 		parent::display($tpl);
 	}
 
@@ -50,41 +75,75 @@ class FabrikViewCrons extends JView
 	 * Add the page title and toolbar.
 	 *
 	 * @since	1.6
+	 *
+	 * @return  void
 	 */
+
 	protected function addToolbar()
 	{
-		require_once JPATH_COMPONENT.'/helpers/fabrik.php';
-		$canDo	= FabrikAdminHelper::getActions($this->state->get('filter.category_id'));
+		require_once JPATH_COMPONENT . '/helpers/fabrik.php';
+		$canDo = FabrikAdminHelper::getActions($this->state->get('filter.category_id'));
 
 		JToolBarHelper::title(JText::_('COM_FABRIK_MANAGER_CRONS'), 'crons.png');
 
-		JToolBarHelper::custom( 'crons.run', 'upload.png', 'upload_f2.png', 'Run');
-		if ($canDo->get('core.create')) {
-			JToolBarHelper::addNew('cron.add','JTOOLBAR_NEW');
+		JToolBarHelper::custom('crons.run', 'upload.png', 'upload_f2.png', 'Run');
+		if ($canDo->get('core.create'))
+		{
+			JToolBarHelper::addNew('cron.add', 'JTOOLBAR_NEW');
 		}
-		if ($canDo->get('core.edit')) {
-			JToolBarHelper::editList('cron.edit','JTOOLBAR_EDIT');
+		if ($canDo->get('core.edit'))
+		{
+			JToolBarHelper::editList('cron.edit', 'JTOOLBAR_EDIT');
 		}
-		if ($canDo->get('core.edit.state')) {
-			if ($this->state->get('filter.state') != 2){
+		if ($canDo->get('core.edit.state'))
+		{
+			if ($this->state->get('filter.state') != 2)
+			{
 				JToolBarHelper::divider();
-				JToolBarHelper::custom('crons.publish', 'publish.png', 'publish_f2.png','JTOOLBAR_PUBLISH', true);
+				JToolBarHelper::custom('crons.publish', 'publish.png', 'publish_f2.png', 'JTOOLBAR_PUBLISH', true);
 				JToolBarHelper::custom('crons.unpublish', 'unpublish.png', 'unpublish_f2.png', 'JTOOLBAR_UNPUBLISH', true);
 			}
 		}
-		if(JFactory::getUser()->authorise('core.manage','com_checkin')) {
+		if (JFactory::getUser()->authorise('core.manage', 'com_checkin'))
+		{
 			JToolBarHelper::custom('crons.checkin', 'checkin.png', 'checkin_f2.png', 'JTOOLBAR_CHECKIN', true);
 		}
-		if ($this->state->get('filter.state') == -2 && $canDo->get('core.delete')) {
-			JToolBarHelper::deleteList('', 'cron.delete','JTOOLBAR_EMPTY_TRASH');
-		} elseif ($canDo->get('core.edit.state')) {
-			JToolBarHelper::trash('crons.trash','JTOOLBAR_TRASH');
+		if ($this->state->get('filter.published') == -2 && $canDo->get('core.delete'))
+		{
+			JToolBarHelper::deleteList('', 'cron.delete', 'JTOOLBAR_EMPTY_TRASH');
 		}
-		if ($canDo->get('core.admin')) {
+		elseif ($canDo->get('core.edit.state'))
+		{
+			JToolBarHelper::trash('crons.trash', 'JTOOLBAR_TRASH');
+		}
+		if ($canDo->get('core.admin'))
+		{
 			JToolBarHelper::divider();
 			JToolBarHelper::preferences('com_fabrik');
 		}
 		JToolBarHelper::divider();
 		JToolBarHelper::help('JHELP_COMPONENTS_FABRIK_CRONS', false, JText::_('JHELP_COMPONENTS_FABRIK_CRONS'));
+
+		if (FabrikWorker::j3())
+		{
+			JHtmlSidebar::setAction('index.php?option=com_fabrik&view=crons');
+
+			$publishOpts = JHtml::_('jgrid.publishedOptions', array('archived' => false));
+			JHtmlSidebar::addFilter(
+			JText::_('JOPTION_SELECT_PUBLISHED'),
+			'filter_published',
+			JHtml::_('select.options', $publishOpts, 'value', 'text', $this->state->get('filter.published'), true)
+			);
+
+			if (!empty($this->packageOptions))
+			{
+				array_unshift($this->packageOptions, JHtml::_('select.option', 'fabrik', JText::_('COM_FABRIK_SELECT_PACKAGE')));
+				JHtmlSidebar::addFilter(
+				JText::_('JOPTION_SELECT_PUBLISHED'),
+				'package',
+				JHtml::_('select.options', $this->packageOptions, 'value', 'text', $this->state->get('com_fabrik.package'), true)
+				);
+			}
+		}
 	}
 }
