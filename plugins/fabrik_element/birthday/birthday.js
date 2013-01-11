12f5d7deb87e03a43953c89d90261099bbe5c9d5
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2013-01-11 04:51:50 +0200

    Making birthday element editable inline
    Same things as for time element.
    https://github.com/Fabrik/fabrik/commit/7357edc661c1793bec5272e5d22fa26ff9f399ca

--- plugins/fabrik_element/birthday/birthday.js
+++ plugins/fabrik_element/birthday/birthday.js
@@ -3,5 +3,27 @@ var FbBirthday = new Class({
 	initialize: function (element, options) {
 		this.plugin = 'birthday';
 		this.parent(element, options);
+	},
+	
+	getValue: function () {
+		var v = [];
+		if (!this.options.editable) {
+			return this.options.value;
+		}
+		this.getElement();
+		
+		this._getSubElements().each(function (f) {
+			v.push(f.get('value'));
+		});
+		return v;
+	},
+	
+	update: function (val) {
+		if (typeOf(val) === 'string') {
+			val = val.split(this.options.separator);
+		}
+		this._getSubElements().each(function (f, x) {
+			f.value = val[x];
+		});
 	}
 });
