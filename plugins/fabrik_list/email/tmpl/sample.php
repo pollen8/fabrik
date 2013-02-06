a0363304c0ef11a305023f712f0e653cc8d5eb66
Hugh Messenger <hugh.messenger@gmail.com>
2013-02-06 17:08:00 -0600

    First cut at porting improvements to list email plugin from 2.1 to 3.x.

---
+++ plugins/fabrik_list/email/tmpl/sample.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @package Joomla
+ * @subpackage Fabrik
+ * @copyright Copyright (C) 2005 Rob Clayburn. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+
+$this->filepath = "foo";
+$formModel = $tableModel->getFormModel();
+foreach ($row as $name => $value) {
+	if (preg_match('#_raw$#', $name)) {
+		continue;
+	}
+	$elementModel =& $formModel->getElement($name);
+	if (empty($elementModel)) {
+		// echo "ooops: $name<br />\n";
+		continue;
+	}
+	$element = $elementModel->getElement();
+	$label = $element->label;
+	$fval = $elementModel->renderTableData($val, $row);
+	echo "$name : $label : $value : $fval<br />\n";
+}
+
+?>
