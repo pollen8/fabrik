f4fa9edd2f61e63b665c31c34acf356c524d9aa8
pollen8 <rob@pollen-8.co.uk>
2013-02-12 12:16:02 +0100

    css to handle long data in merged repeatdata - you may need to tweak the max-width setting for specific list

--- components/com_fabrik/views/list/tmpl/bootstrap/template_css.php
+++ components/com_fabrik/views/list/tmpl/bootstrap/template_css.php
@@ -12,4 +12,15 @@ clear:both;
 .fabrikDataContainer .pagination a{
 	float: left;
 }
+
+ul.fabrikRepeatData {
+	list-style-position:inside;
+	margin: 0 0 0 6px;
+}
+.fabrikRepeatData > li {
+	white-space: nowrap;
+	max-width:350px;
+	overflow:hidden;
+	text-overflow: ellipsis;
+}
 ";?>
