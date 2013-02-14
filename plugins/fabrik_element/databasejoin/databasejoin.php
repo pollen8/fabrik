2b1f91b430ca88b9ce9a63a9dd80e5129f420e34
Hugh Messenger <hugh.messenger@gmail.com>
2013-02-13 20:39:45 -0600

    Band aid in join element for PHP warning issue caused by inline edits. Also change to en-GB language, to make it clear that the join value MUST be unique.

--- plugins/fabrik_element/databasejoin/databasejoin.php
+++ plugins/fabrik_element/databasejoin/databasejoin.php
@@ -238,7 +238,9 @@ class plgFabrik_ElementDatabasejoin extends plgFabrik_ElementList
 		$params = $this->getParams();
 		$db = $this->getDb();
 		$join = $this->getJoin();
-		if (($params->get($this->concatLabelParam) != '') && JRequest::getVar('overide_join_val_column_concat') != 1)
+		// $$$ hugh - bandaid for inlineedit, problem where $join isn't loaded, as per comments in getJoin().
+		// for now, just avoid this code if $join isn't an object.
+		if (is_object($join) && ($params->get($this->concatLabelParam) != '') && JRequest::getVar('overide_join_val_column_concat') != 1)
 		{
 			$val = str_replace("{thistable}", $join->table_join_alias, $params->get($this->concatLabelParam));
 			$w = new FabrikWorker;
