add715e1371bb90e4592817c88aa931328524519
pollen8 <rob@pollen-8.co.uk>
2013-03-27 17:12:58 +0100

    improved form buttons edit page layout

--- administrator/components/com_fabrik/views/form/tmpl/bootstrap_buttons.php
+++ administrator/components/com_fabrik/views/form/tmpl/bootstrap_buttons.php
@@ -11,12 +11,70 @@
 ?>
 <div class="tab-pane" id="tab-buttons">
 
-	<fieldset class="form-horizontal">
-		<legend><?php echo JText::_('COM_FABRIK_BUTTONS');?></legend>
-		<?php foreach ($this->form->getFieldset('buttons') as $this->field) :
-			echo $this->loadTemplate('control_group');
-		endforeach;
-		?>
-	</fieldset>
+<div class="row-fluid">
+	<div class="span6">
+		<fieldset class="form-horizontal">
+			<legend><?php echo JText::_('COM_FABRIK_RESET');?></legend>
+			<?php foreach ($this->form->getFieldset('buttons-reset') as $this->field) :
+				echo $this->loadTemplate('control_group');
+			endforeach;
+			?>
+		</fieldset>
+	</div>
 
+	<div class="span6">
+		<fieldset class="form-horizontal">
+			<legend><?php echo JText::_('COM_FABRIK_COPY');?></legend>
+			<?php foreach ($this->form->getFieldset('buttons-copy') as $this->field) :
+				echo $this->loadTemplate('control_group');
+			endforeach;
+			?>
+		</fieldset>
+	</div>
+
+</div>
+
+<div class="row-fluid">
+	<div class="span6">
+		<fieldset class="form-horizontal">
+			<legend><?php echo JText::_('COM_FABRIK_BACK');?></legend>
+			<?php foreach ($this->form->getFieldset('buttons-goback') as $this->field) :
+				echo $this->loadTemplate('control_group');
+			endforeach;
+			?>
+		</fieldset>
+	</div>
+
+	<div class="span6">
+		<fieldset class="form-horizontal">
+			<legend><?php echo JText::_('COM_FABRIK_APPLY');?></legend>
+			<?php foreach ($this->form->getFieldset('buttons-apply') as $this->field) :
+				echo $this->loadTemplate('control_group');
+			endforeach;
+			?>
+		</fieldset>
+	</div>
+
+</div>
+
+<div class="row-fluid">
+	<div class="span6">
+		<fieldset class="form-horizontal">
+			<legend><?php echo JText::_('COM_FABRIK_SAVE');?></legend>
+			<?php foreach ($this->form->getFieldset('buttons-save') as $this->field) :
+				echo $this->loadTemplate('control_group');
+			endforeach;
+			?>
+		</fieldset>
+	</div>
+	<div class="span6">
+		<fieldset class="form-horizontal">
+			<legend><?php echo JText::_('COM_FABRIK_DELETE');?></legend>
+			<?php foreach ($this->form->getFieldset('buttons-delete') as $this->field) :
+				echo $this->loadTemplate('control_group');
+			endforeach;
+			?>
+		</fieldset>
+	</div>
+</div>
 </div>
