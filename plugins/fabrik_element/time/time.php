d03ebb8be59dcbe3d24042a504b7c2ccf9f297e0
pollen8 <rob@pollen-8.co.uk>
2013-04-15 11:05:33 +0200

    fixed - time plugin offset notice when formatting not hh:mm:ss

--- plugins/fabrik_element/time/time.php
+++ plugins/fabrik_element/time/time.php
@@ -143,8 +143,7 @@ class plgFabrik_ElementTime extends plgFabrik_Element
 	}
 
 	/**
-	 * can be overwritten by plugin class
-	 * determines the value for the element in the form view
+	 * Determines the value for the element in the form view
 	 *
 	 * @param   array  $data           Data
 	 * @param   int    $repeatCounter  When repeating joinded groups we need to know what part of the array to access
@@ -423,7 +422,10 @@ class plgFabrik_ElementTime extends plgFabrik_Element
 		{
 			if ($d)
 			{
-				list($hour, $min, $sec) = explode(':', $d);
+				$bits = explode(':', $d);
+				$hour = JArrayHelper::getValue($bits, 0, '00');
+				$min = JArrayHelper::getValue($bits, 1, '00');
+				$sec = JArrayHelper::getValue($bits, 2, '00');
 				$hms = $hour . $sep . $min . $sep . $sec;
 				$hm = $hour . $sep . $min;
 				$ms = $min . $sep . $sec;
