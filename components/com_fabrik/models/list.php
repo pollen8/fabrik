66298a60bcf2391abd36fdc04c478b7cd5594123
pollen8 <rob@pollen-8.co.uk>
2013-04-25 11:46:15 +0200

    removing debug text

--- components/com_fabrik/models/list.php
+++ components/com_fabrik/models/list.php
@@ -4794,7 +4794,6 @@ class FabrikFEModelList extends JModelForm
 		// $$$ rob getData() should always be run first
 		if (is_null($this->_data))
 		{
-			echo "list gettotal records going to call getData() <br>";
 			$this->getData();
 			return $this->totalRecords;
 		}
