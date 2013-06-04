04fdfee68b37e192675e3000136ed1da73842f76
pollen8 <rob@pollen-8.co.uk>
2013-06-04 16:08:41 +0200

    fixed: tab tmpl missing tips above/side/below

--- components/com_fabrik/views/form/tmpl/tabs/default_group.php
+++ components/com_fabrik/views/form/tmpl/tabs/default_group.php
@@ -10,21 +10,33 @@
  */
 ?>
 <ul>
-<?php foreach ($this->elements as $element)
-{
+<?php foreach ($this->elements as $element) :
+	if ($this->tipLocation == 'above' && $element->tipAbove != '') :?>
+		<li><?php echo $element->tipAbove; ?></li>
+	<?php
+	endif;
 	?>
-	<li <?php echo @$element->column;?> class="<?php echo $element->containerClass;?>">
-		<?php echo $element->label;?>
+	<li <?php echo @$element->column;?> class="<?php echo $element->containerClass; ?>">
+		<?php echo $element->label; ?>
 		<?php echo $element->errorTag; ?>
 		<div class="fabrikElement">
-			<?php echo $element->element;?>
-
+			<?php echo $element->element; ?>
 		</div>
+		<?php if ($this->tipLocation == 'side') :
+			echo $element->tipSide;
+		endif;
+		?>
+
 		<div class="fabrikErrorMessage">
 				<?php echo $element->error;?>
 			</div>
 		<div style="clear:both"></div>
 	</li>
 	<?php
-}?>
+		if ($this->tipLocation == 'below' && $element->tipBelow != '') :?>
+		<li><?php echo $element->tipBelow; ?></li>
+	<?php
+	endif;
+endforeach;
+?>
 </ul>
