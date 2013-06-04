9c96841e01f679e7b85f8922cad9eca6603b0da0
pollen8 <rob@pollen-8.co.uk>
2013-06-04 10:28:45 +0200

    fixed: bootstrap class missing from dbjoin autocomplete

--- plugins/fabrik_element/databasejoin/databasejoin.php
+++ plugins/fabrik_element/databasejoin/databasejoin.php
@@ -1240,9 +1240,10 @@ class PlgFabrik_ElementDatabasejoin extends PlgFabrik_ElementList
 		{
 			$label = (array) $this->getLabelForValue($label[0], $label[0], $repeatCounter);
 		}
+		$class = 'class="fabrikinput inputbox autocomplete-trigger ' . $params->get('bootstrap_class', 'input-large') . '"';
 		$autoCompleteName = str_replace('[]', '', $thisElName) . '-auto-complete';
 		$html[] = '<input type="text" size="' . $params->get('dbjoin_autocomplete_size', '20') . '" name="' . $autoCompleteName . '" id="' . $id
-		. '-auto-complete" value="' . JArrayHelper::getValue($label, 0) . '" class="fabrikinput inputbox autocomplete-trigger"/>';
+		. '-auto-complete" value="' . JArrayHelper::getValue($label, 0) . '"' . $class . '"/>';
 
 		// $$$ rob - class property required when cloning repeat groups - don't remove
 		$html[] = '<input type="hidden" tabindex="-1" class="fabrikinput" name="' . $thisElName . '" id="' . $id . '" value="'
