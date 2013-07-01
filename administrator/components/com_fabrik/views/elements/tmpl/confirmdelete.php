0c4c0fa06d4fb7d8269a46750ff8757fa6d56c52
pollen8 <rob@pollen-8.co.uk>
2013-07-01 17:57:03 +0200

    fixes # #765 - delete elements from trash - you had to select each element individually if you wanted to remove its field from the db, replaced with checkbox and checkall option

--- administrator/components/com_fabrik/views/elements/tmpl/confirmdelete.php
+++ administrator/components/com_fabrik/views/elements/tmpl/confirmdelete.php
@@ -14,28 +14,26 @@ defined('_JEXEC') or die;
 ?>
 
 <form action="<?php JRoute::_('index.php?option=com_fabrik'); ?>" method="post" name="adminForm" id="fabrik-form" class="form-validate">
-	<fieldset class="adminform">
-		<legend><?php echo JText::_('COM_FABRIK_ELEMENTS');?></legend>
-		<ul class="adminformlist">
-		<?php for ($i = 0; $i < count($this->items); $i++)
-		{
-		$element = $this->items[$i];?>
-  		<li>
-  			<label for="drop-<?php echo $element->id?>">
-  				<?php echo $element->label; ?>
-  			</label>
-	  		<fieldset id="drop-<?php echo $element->id?>" class="radio">
-		  		<label><input type="radio" name="drop[<?php echo $element->id ?>][]" value="0" checked="checked" /><?php echo JText::_('JNO') ?></label>
-					<label><input type="radio" name="drop[<?php echo $element->id ?>][]" value="1" /><?php echo JText::_('JYES') ?></label>
-				</fieldset>
-				<input type="hidden" name="cid[]" value="<?php echo $element->id ?>" />
-  		</li>
-<?php
-}
-?>
-		</ul>
-
-	</fieldset>
+	<table class="adminlist">
+		<thead>
+			<tr>
+				<th style="width:2%"><input type="checkbox" name="toggle" value="" onclick="checkAll(this);" /></th>
+				<th><?php echo JText::_('COM_FABRIK_ELEMENTS'); ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php for ($i = 0; $i < count($this->items); $i++) :
+				$element = $this->items[$i];?>
+			<tr>
+				<td>
+					<?php echo JHtml::_('grid.id', $i, $element->id, false, 'elementIds'); ?>
+					<input type="hidden" name="cid[]" value="<?php echo $element->id?>" />
+				</td>
+				<td><?php echo $element->label; ?></td>
+			</tr>
+			<?php endfor?>
+		</tbody>
+	</table>
 	<input type="hidden" name="task" value="" />
   	<?php echo JHTML::_('form.token');
 	echo JHTML::_('behavior.keepalive'); ?>
