5e1962016a86d77ca04d365dd3b2234a13814271
pollen8 <rob@pollen-8.co.uk>
2013-07-05 16:16:16 +0200

    admin form mod not using formid from params

--- administrator/modules/mod_fabrik_form/mod_fabrik_form.php
+++ administrator/modules/mod_fabrik_form/mod_fabrik_form.php
@@ -32,7 +32,7 @@ $input->set('layout', $origLayout);
 JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_fabrik/tables');
 JModelLegacy::addIncludePath(COM_FABRIK_FRONTEND . '/models', 'FabrikFEModel');
 
-$formId	= (int) $params->get('form_id', 1);
+$formId	= (int) $params->get('formid', 1);
 $rowid = (int) $params->get('row_id', 0);
 $layout = $params->get('template', '');
 $usersConfig = JComponentHelper::getParams('com_fabrik');
