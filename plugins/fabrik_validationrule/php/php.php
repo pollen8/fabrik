7cab8b664c994228d7740125ff2dfaeae9afc6e5
Hugh Messenger <hugh.messenger@gmail.com>
2013-07-22 18:14:12 -0500

    Fix for misleading eval err msg in PHP validation

--- plugins/fabrik_validationrule/php/php.php
+++ plugins/fabrik_validationrule/php/php.php
@@ -96,7 +96,7 @@ class PlgFabrik_ValidationrulePhp extends PlgFabrik_Validationrule
 		$phpCode = $params->get('php-code');
 		$phpCode = $w->parseMessageForPlaceHolder($phpCode, $formData, true, true);
 		$retval = @eval($phpCode);
-		FabrikWorker::logEval($retval, 'Caught exception on php validation of ' . $elementModel->getFullName(false, false) . '::_getV(): %s');
+		FabrikWorker::logEval($retval, 'Caught exception on php validation of ' . $elementModel->getFullName(true, false) . ': %s');
 		return $retval;
 	}
 }
