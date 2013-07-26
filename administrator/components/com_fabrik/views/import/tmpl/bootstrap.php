1e415c6282f7a163b14f1b8ac1d5290ac0aa5a3e
pollen8 <rob@pollen-8.co.uk>
2013-07-26 13:48:55 +0200

    fixed: warnings in admin when importing a new list via csv

--- administrator/components/com_fabrik/views/import/tmpl/bootstrap.php
+++ administrator/components/com_fabrik/views/import/tmpl/bootstrap.php
@@ -72,8 +72,9 @@ window.addEvent('domready', function () {
 		?>
 	</fieldset>
 	<?php endforeach;?>
-
-	<input type="hidden" name="task" value="" />
+	<input type="hidden" name="jform[drop_data]" value="0" />
+	<input type="hidden" name="jform[overwrite]" value="0" />
+ 	<input type="hidden" name="task" value="" />
   	<?php echo JHTML::_('form.token');
 	echo JHTML::_('behavior.keepalive'); ?>
 	</div>
