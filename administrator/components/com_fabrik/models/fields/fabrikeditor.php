9a2915effd440aa0cc15af69e939be3cc9983aa5
Sophist <github@sodalis.co.uk>
2013-09-01 21:59:24 +0100

    Make fabrikeditor variable height
    Fabrikeditor will now grow to max-height and shrink back to original
    height. Default max-height is twice original height.
    
    Also de-duplicated code.

--- administrator/components/com_fabrik/models/fields/fabrikeditor.php
+++ administrator/components/com_fabrik/models/fields/fabrikeditor.php
@@ -44,25 +44,36 @@ class JFormFieldFabrikeditor extends JFormFieldTextArea
 
 	protected function getInput()
 	{
+		// Initialize some field attributes.
+		$class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
+		$disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
+		$columns = $this->element['cols'] ? ' cols="' . (int) $this->element['cols'] . '"' : '';
+		$rows = $this->element['rows'] ? ' rows="' . (int) $this->element['rows'] . '"' : '';
+		$required = $this->required ? ' required="required" aria-required="true"' : '';
+
+		// JS events are saved as encoded html - so we don't want to double encode them
+		$encoded = JArrayHelper::getValue($this->element, 'encoded', false);
+		if (!$encoded)
+		{
+			$this->value = htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8');
+		}
+
+		$onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
+		$editor = '<textarea name="' . $this->name . '" id="' . $this->id . '"'
+			. $columns . $rows . $class . $disabled . $onchange . $required . '>'
+			. $this->value . '</textarea>';
+
 		$version = new JVersion;
 		if ($version->RELEASE == 2.5)
 		{
-			// Initialize some field attributes.
-			$class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
-			$disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
-			$columns = $this->element['cols'] ? ' cols="' . (int) $this->element['cols'] . '"' : '';
-			$rows = $this->element['rows'] ? ' rows="' . (int) $this->element['rows'] . '"' : '';
-			$required = $this->required ? ' required="required" aria-required="true"' : '';
-
-			// Initialize JavaScript field attributes.
-			$onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
-
-			return '<textarea name="' . $this->name . '" id="' . $this->id . '"' . $columns . $rows . $class . $disabled . $onchange . $required . '>'
-				. $this->value . '</textarea>';
+			return $editor;
 		}
+
+		// Joomla 3 version
 		$mode = $this->element['mode'] ? (string) $this->element['mode'] : 'html';
 		$theme = $this->element['theme'] ? (string) $this->element['theme'] : 'github';
 		$height = $this->element['height'] ? (string) $this->element['height'] : '200px';
+		$maxHeight = $this->element['max-height'] ? (string) $this->element['max-height'] : str_ireplace('px', '', $height) * 2 . 'px';
 		$width = $this->element['width'] ? (string) $this->element['width'] : '300px';
 		FabrikHelperHTML::framework();
 		FabrikHelperHTML::iniRequireJS();
@@ -76,26 +87,47 @@ class JFormFieldFabrikeditor extends JFormFieldTextArea
 			$aceMode = '"ace/mode/' . $mode . '"';
 		}
 
+		$minHeight = str_ireplace('px', '' , $height);
+		$maxHeight = str_ireplace('px', '' , $maxHeight);
+		// In code below, the +/- 2 is to account for the top/bottom border of 1px each.
 		$script = '
-			window.addEvent(\'domready\', function () {
-			var MyEditor = ace.edit("' . $this->id . '-ace");
-			MyEditor.setTheme("ace/theme/' . $theme . '");
-			MyEditor.getSession().setMode(' . $aceMode . ');
-			MyEditor.setValue(document.id("' . $this->id . '").value);
-			MyEditor.setAnimatedScroll(true);
-			MyEditor.setBehavioursEnabled(true);
-			MyEditor.setDisplayIndentGuides(true);
-			MyEditor.setHighlightGutterLine(true);
-			MyEditor.setHighlightSelectedWord(true);
-			MyEditor.setShowFoldWidgets(true);
-			MyEditor.setWrapBehavioursEnabled(true);
-			MyEditor.getSession().setUseWrapMode(true);
-			MyEditor.getSession().setTabSize(2);
-			MyEditor.on("blur", function () {
-				document.id("' . $this->id . '").value = MyEditor.getValue();
-			});
-			});
-			';
+window.addEvent(\'domready\', function () {
+	var FbEditor = ace.edit("' . $this->id . '-ace");
+	FbEditor.setTheme("ace/theme/' . $theme . '");
+	FbEditor.getSession().setMode(' . $aceMode . ');
+	FbEditor.setValue(document.id("' . $this->id . '").value);
+	FbEditor.setAnimatedScroll(true);
+	FbEditor.setBehavioursEnabled(true);
+	FbEditor.setDisplayIndentGuides(true);
+	FbEditor.setHighlightGutterLine(true);
+	FbEditor.setHighlightSelectedWord(true);
+	FbEditor.setShowFoldWidgets(true);
+	FbEditor.setWrapBehavioursEnabled(true);
+	FbEditor.getSession().setUseWrapMode(true);
+	FbEditor.getSession().setTabSize(2);
+	FbEditor.on("blur", function () {
+		document.id("' . $this->id . '").value = FbEditor.getValue();
+	});
+	var maxlines = Math.floor((' . $maxHeight . ' - 2) / FbEditor.renderer.lineHeight);
+	var updateHeight = function () {
+		var s = FbEditor.getSession();
+		var r = FbEditor.renderer;
+		var l = s.getScreenLength();
+		var h = (l > maxlines ? maxlines : l)
+		      * r.lineHeight
+		      + (r.$horizScroll ? r.scrollBar.getWidth() : 0)
+		      + 2;
+		h = h < ' . $minHeight . ' ? ' . $minHeight . ' : h;
+		c = document.id("' . $this->id . '-container").getStyle("height").toInt();
+		if (c !== h) {
+			document.id("' . $this->id . '-container").setStyle("height", h.toString() + "px");
+			FbEditor.resize();
+		}
+	}
+	updateHeight();
+	FbEditor.getSession().on("change", updateHeight);
+});
+		';
 
 		$src = array('media/com_fabrik/js/lib/ace/src-min-noconflict/ace.js');
 		FabrikHelperHTML::script($src, $script);
@@ -119,26 +151,6 @@ class JFormFieldFabrikeditor extends JFormFieldTextArea
 </style>';
 		$this->element['cols'] = 1;
 		$this->element['rows'] = 1;
-
-		// Initialize some field attributes.
-		$class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
-		$disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
-		$columns = $this->element['cols'] ? ' cols="' . (int) $this->element['cols'] . '"' : '';
-		$rows = $this->element['rows'] ? ' rows="' . (int) $this->element['rows'] . '"' : '';
-		$required = $this->required ? ' required="required" aria-required="true"' : '';
-
-		// Initialize JavaScript field attributes.
-		$onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
-
-		// JS events are saved as encoded html - so we don't want to double encode them
-		$encoded = JArrayHelper::getValue($this->element, 'encoded', false);
-		if (!$encoded)
-		{
-			$this->value = htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8');
-		}
-
-		$editor = '<textarea name="' . $this->name . '" id="' . $this->id . '"' . $columns . $rows . $class . $disabled . $onchange . $required . '>'
-			. $this->value . '</textarea>';
 
 		// For element js event code.
 		return '<div id="' . $this->id . '-container"><div id="' . $this->id . '-ace"></div>' . $editor . '</div>';
