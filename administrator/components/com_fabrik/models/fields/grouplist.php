6e50dfb545f77edc1b2397ffc3fef3244372d0eb
pollen8 <rob@pollen-8.co.uk>
2013-09-02 13:47:03 +0200

    changed : group list now contains groupopts labled with form labels

--- administrator/components/com_fabrik/models/fields/grouplist.php
+++ administrator/components/com_fabrik/models/fields/grouplist.php
@@ -16,7 +16,7 @@ require_once JPATH_ADMINISTRATOR . '/components/com_fabrik/helpers/element.php';
 jimport('joomla.html.html');
 jimport('joomla.form.formfield');
 jimport('joomla.form.helper');
-JFormHelper::loadFieldClass('list');
+JFormHelper::loadFieldClass('groupedlist');
 
 /**
  * Renders a list of groups
@@ -26,7 +26,7 @@ JFormHelper::loadFieldClass('list');
  * @since       1.6
  */
 
-class JFormFieldGroupList extends JFormFieldList
+class JFormFieldGroupList extends JFormFieldGroupedList
 {
 	/**
 	 * Element name
@@ -42,7 +42,7 @@ class JFormFieldGroupList extends JFormFieldList
 	 * @return  string	The field input markup.
 	 */
 
-	protected function getOptions()
+	protected function getGroups()
 	{
 
 		if ($this->value == '')
@@ -56,17 +56,34 @@ class JFormFieldGroupList extends JFormFieldList
 		$db = FabrikWorker::getDbo(true);
 		$query = $db->getQuery(true);
 
-		$query->select('id AS value, name AS text');
+		$query->select('g.id AS value, g.name AS text, f.label AS form');
 		$query->from('#__{package}_groups AS g');
-		$query->where('published <> -2');
-		$query->order('name');
+		$query->where('g.published <> -2')
+		->join('INNER', '#__{package}_formgroup AS fg ON fg.group_id = g.id')
+		->join('INNER', '#__{package}_forms AS f on fg.form_id = f.id');
+		$query->order('f.label, g.name');
 
 		// Get the options.
 		$db->setQuery($query);
-
 		$options = $db->loadObjectList();
-		array_unshift($options, JHtml::_('select.option', '', JText::_('COM_FABRIK_PLEASE_SELECT')));
-		return $options;
+
+		$groups = array();
+		foreach ($options as $option)
+		{
+			if (!array_key_exists($option->form, $groups))
+			{
+				$groups[$option->form] = array();
+			}
+			$groups[$option->form][] = $option;
+		}
+
+		// Check for a database error.
+		if ($db->getErrorNum())
+		{
+			JError::raiseWarning(500, $db->getErrorMsg());
+		}
+		array_unshift($groups, JHtml::_('select.option', '', JText::_('COM_FABRIK_PLEASE_SELECT')));
+		return $groups;
 	}
 
 }
