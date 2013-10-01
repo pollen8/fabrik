c994620d13b9a8e38c75684066d6936c2a89d2fb
Hugh Messenger <hugh.messenger@gmail.com>
2013-10-01 12:18:53 -0500

    Little fix in upload element, where we were calling loadMeForAjax() twice.

--- plugins/fabrik_element/fileupload/fileupload.php
+++ plugins/fabrik_element/fileupload/fileupload.php
@@ -2450,7 +2450,7 @@ class PlgFabrik_ElementFileupload extends PlgFabrik_Element
 	{
 		$app = JFactory::getApplication();
 		$input = $app->input;
-		$this->loadMeForAjax();
+		//$this->loadMeForAjax();
 		$this->setId($input->getInt('element_id'));
 		$this->loadMeForAjax();
 		$this->getElement();
