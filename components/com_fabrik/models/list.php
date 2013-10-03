f5a43553e7cd29e82cf0e3a37b5b31d83f229e10
Hugh Messenger <hugh.messenger@gmail.com>
2013-10-03 11:50:43 -0500

    Fix for & in group by data, may not be an issue in 3.1, but just had to fix it in 3.0, won't hurt in 3.1, so just in case.

--- components/com_fabrik/models/list.php
+++ components/com_fabrik/models/list.php
@@ -1197,7 +1197,8 @@ class FabrikFEModelList extends JModelForm
 			{
 				if (isset($data[$i]->$groupBy))
 				{
-					$sdata = $data[$i]->$groupBy;
+					// get rid of & as it blows up SimpleXMLElement, and dont want to use htmlspecialchars as don't want to mess with <, >, etc.
+					$sdata = str_replace('&', '&amp;', str_replace('&amp;', '&', $data[$i]->$groupBy));
 
 					// Test if its just an <a>*</a> tag - if so allow HTML (enables use of icons)
 					$xml = new SimpleXMLElement('<div>' . $sdata . '</div>');
