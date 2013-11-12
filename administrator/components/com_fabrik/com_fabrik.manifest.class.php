b643e2f170566524f21b3567ed3eca639a6535a7
pollen8 <rob@pollen-8.co.uk>
2013-11-12 18:58:09 +0100

    remove old update site

--- administrator/components/com_fabrik/com_fabrik.manifest.class.php
+++ administrator/components/com_fabrik/com_fabrik.manifest.class.php
@@ -273,10 +273,11 @@ class Com_FabrikInstallerScript
 	public function postflight($type, $parent)
 	{
 		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
 
-		// Remove old update site
-		$db->setQuery("DELETE FROM #__update_sites WHERE location LIKE '%update/component/com_fabrik%'");
-		$db->execute();
+		// Remove old update site & Fabrik 3.0.x update site
+		$query->delete('#__update_sites')->where("location LIKE '%update/component/com_fabrik%' OR location = 'http://fabrikar.com/update/fabrik/package_list.xml'");
+		$db->setQuery($query);
 
 		if (!$db->execute())
 		{
