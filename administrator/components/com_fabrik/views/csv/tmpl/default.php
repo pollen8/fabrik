118a47684444cc5acbd8ac00a6f782654fa6efe7
pollen8 <rob@pollen-8.co.uk>
2013-11-28 16:16:00 +0100

    added: backend csv export view#

---
+++ administrator/components/com_fabrik/views/csv/tmpl/default.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* @package Joomla
+* @subpackage Fabrik
+* @copyright Copyright (C) 2005 Rob Clayburn. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+*/
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+?>
+<form name="listform_<?php echo $this->id?>" id="listform_<?php echo $this->id?>">
+</form>
+<!-- secret hihi hello linh! (H) -->
