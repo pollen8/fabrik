cca691224594e86ea60c91be66eaa5393c47ecc9
Sophist <github@sodalis.co.uk>
2014-01-06 11:23:15 +0000

    Fix typo .table -> _table

--- components/com_fabrik/views/details/tmpl/bootstrap_tabs/default.php
+++ components/com_fabrik/views/details/tmpl/bootstrap_tabs/default.php
@@ -122,7 +122,7 @@ echo $this->plugintop;
 				/* Load the group template - this can be :
 				 *  * default_group.php - standard group non-repeating rendered as an unordered list
 				 *  * default_repeatgroup.php - repeat group rendered as an unordered list
-				 *  * default_repeatgroup.table.php - repeat group rendered in a table.
+				 *  * default_repeatgroup_table.php - repeat group rendered in a table.
 				 */
 				$this->elements = $group->elements;
 				echo $this->loadTemplate($group->tmpl);
