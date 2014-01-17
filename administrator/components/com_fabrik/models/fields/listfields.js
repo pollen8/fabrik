eab084b975241de84af2afc7db9cc7611ae9a074
pollen8 <rob@pollen-8.co.uk>
2014-01-17 13:03:33 +0100

    fixed: listfields JElement js - insert into element name into textarea not working in chrome

--- administrator/components/com_fabrik/models/fields/listfields.js
+++ administrator/components/com_fabrik/models/fields/listfields.js
@@ -213,80 +213,80 @@ var ListFieldsElement = new Class({
 	 * Start of text insertion code - taken from 
 	 * http://stackoverflow.com/questions/3510351/how-do-i-add-text-to-a-textarea-at-the-cursor-location-using-javascript
 	 */
-	getInputSelection: function(el) {
-	    var start = 0, end = 0, normalizedValue, range,
-	        textInputRange, len, endRange;
+	getInputSelection: function (el) {
+		var start = 0, end = 0, normalizedValue, range,
+		textInputRange, len, endRange;
 
-	    if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
-	        start = el.selectionStart;
-	        end = el.selectionEnd;
-	    } else {
-	        range = document.selection.createRange();
+		if (typeof el.selectionStart === "number" && typeof el.selectionEnd === "number") {
+			start = el.selectionStart;
+			end = el.selectionEnd;
+		} else {
+			range = document.selection.createRange();
 
-	        if (range && range.parentElement() == el) {
-	            len = el.value.length;
-	            normalizedValue = el.value.replace(/\r\n/g, "\n");
+			if (range && range.parentElement() === el) {
+				len = el.value.length;
+				normalizedValue = el.value.replace(/\r\n/g, "\n");
 
-	            // Create a working TextRange that lives only in the input
-	            textInputRange = el.createTextRange();
-	            textInputRange.moveToBookmark(range.getBookmark());
+				// Create a working TextRange that lives only in the input
+				textInputRange = el.createTextRange();
+				textInputRange.moveToBookmark(range.getBookmark());
 
-	            // Check if the start and end of the selection are at the very end
-	            // of the input, since moveStart/moveEnd doesn't return what we want
-	            // in those cases
-	            endRange = el.createTextRange();
-	            endRange.collapse(false);
+				// Check if the start and end of the selection are at the very end
+				// of the input, since moveStart/moveEnd doesn't return what we want
+				// in those cases
+				endRange = el.createTextRange();
+				endRange.collapse(false);
 
-	            if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
-	                start = end = len;
-	            } else {
-	                start = -textInputRange.moveStart("character", -len);
-	                start += normalizedValue.slice(0, start).split("\n").length - 1;
+				if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+					start = end = len;
+				} else {
+					start = -textInputRange.moveStart("character", -len);
+					start += normalizedValue.slice(0, start).split("\n").length - 1;
 
-	                if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
-	                    end = len;
-	                } else {
-	                    end = -textInputRange.moveEnd("character", -len);
-	                    end += normalizedValue.slice(0, end).split("\n").length - 1;
-	                }
-	            }
-	        }
-	    }
+					if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
+						end = len;
+					} else {
+						end = -textInputRange.moveEnd("character", -len);
+						end += normalizedValue.slice(0, end).split("\n").length - 1;
+					}
+				}
+			}
+		}
 
-	    return {
-	        start: start,
-	        end: end
-	    };
+		return {
+			start: start,
+			end: end
+		};
 	},
 	
 	offsetToRangeCharacterMove: function (el, offset) {
-	    return offset - (el.value.slice(0, offset).split("\r\n").length - 1);
+		return offset - (el.value.slice(0, offset).split("\r\n").length - 1);
 	},
 
 	setSelection: function (el, start, end) {
-	    if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
-	        el.selectionStart = start;
-	        el.selectionEnd = end;
-	    } else if (typeof el.createTextRange != "undefined") {
-	        var range = el.createTextRange();
-	        var startCharMove = this.offsetToRangeCharacterMove(el, start);
-	        range.collapse(true);
-	        if (start == end) {
-	            range.move("character", startCharMove);
-	        } else {
-	            range.moveEnd("character", this.offsetToRangeCharacterMove(el, end));
-	            range.moveStart("character", startCharMove);
-	        }
-	        range.select();
-	    }
+		if (typeof el.selectionStart === "number" && typeof el.selectionEnd === "number") {
+			el.selectionStart = start;
+			el.selectionEnd = end;
+		} else if (typeof el.createTextRange !== "undefined") {
+			var range = el.createTextRange();
+			var startCharMove = this.offsetToRangeCharacterMove(el, start);
+			range.collapse(true);
+			if (start === end) {
+				range.move("character", startCharMove);
+			} else {
+				range.moveEnd("character", this.offsetToRangeCharacterMove(el, end));
+				range.moveStart("character", startCharMove);
+			}
+			range.select();
+		}
 	},
 
 	insertTextAtCaret: function (el, text) {
-	    var pos = this.getInputSelection(el).end;
-	    var newPos = pos + text.length;
-	    var val = el.value;
-	    el.value = val.slice(0, pos) + text + val.slice(pos);
-	    this.setSelection(el, newPos, newPos);
+		var pos = this.getInputSelection(el).end;
+		var newPos = pos + text.length;
+		var val = el.value;
+		el.value = val.slice(0, pos) + text + val.slice(pos);
+		this.setSelection(el, newPos, newPos);
 	}
 });
 
