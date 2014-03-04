83df02c25739efed1b077f89b41fa1d4e41aceb6
Hugh Messenger <hugh.messenger@gmail.com>
2014-03-04 00:06:22 -0600

    Fixing rating element, issue with $ids[] and caching.

--- plugins/fabrik_element/rating/rating.php
+++ plugins/fabrik_element/rating/rating.php
@@ -85,7 +85,9 @@ class PlgFabrik_ElementRating extends PlgFabrik_Element
 
 		if ($params->get('rating-mode') !== 'creator-rating')
 		{
-			list($data, $total) = $this->getRatingAverage($data, $listid, $formid, $row_id);
+			$d = $this->getListModel()->getData();
+			$ids = JArrayHelper::getColumn($d, '__pk_val');
+			list($data, $total) = $this->getRatingAverage($data, $listid, $formid, $row_id, $ids);
 		}
 
 		$app = JFactory::getApplication();
@@ -201,6 +203,7 @@ class PlgFabrik_ElementRating extends PlgFabrik_Element
 			$query->select('row_id, AVG(rating) AS r, COUNT(rating) AS total')->from(' #__{package}_ratings')
 				->where(array('rating <> -1', 'listid = ' . (int) $listid, 'formid = ' . (int) $formid, 'element_id = ' . (int) $elementid))
 				->where('row_id IN (' . implode(',', $ids) . ')')->group('row_id');
+			$squery = (string) $query;
 
 			// Do this  query so that list view only needs one query to load up all ratings
 			$db->setQuery($query);
