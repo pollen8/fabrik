a7a6d19e38c48c0ba713e7a2c406f4e82cb8f688
Hugh Messenger <hugh.messenger@gmail.com>
2014-04-12 17:23:35 -0500

    Need to use JText not FText when complaining about Fabrik system plugin not loaded, as in that case the FText class won't be there.  Fixes #1227.

--- components/com_fabrik/fabrik.php
+++ components/com_fabrik/fabrik.php
@@ -16,7 +16,7 @@ jimport('joomla.filesystem.file');
 
 if (!defined('COM_FABRIK_FRONTEND'))
 {
-	throw new RuntimeException(FText::_('COM_FABRIK_SYSTEM_PLUGIN_NOT_ACTIVE'), 400);
+	throw new RuntimeException(JText::_('COM_FABRIK_SYSTEM_PLUGIN_NOT_ACTIVE'), 400);
 }
 
 jimport('joomla.log.log');
