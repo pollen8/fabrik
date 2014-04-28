f883db6264bc7f199ecddd6ba571e24ffa450306
Hugh Messenger <hugh.messenger@gmail.com>
2014-04-28 13:07:30 -0500

    Trimming root folder from image link in list display

--- plugins/fabrik_element/image/image.php
+++ plugins/fabrik_element/image/image.php
@@ -171,6 +171,7 @@ class PlgFabrik_ElementImage extends PlgFabrik_Element
 				else
 				{
 					$data[$i] = JString::ltrim($data[$i], '/');
+					$data[$i] = FabrikString::ltrimword($data[$i], $selectImage_root_folder);
 					$src = COM_FABRIK_LIVESITE . $selectImage_root_folder . $data[$i];
 				}
 
