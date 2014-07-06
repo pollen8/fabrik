dd52c5e1d6374006665d17c6ea67da42a0f2610d
Hugh Messenger <hugh.messenger@gmail.com>
2014-07-05 21:10:57 -0500

    Updated upload element's audio model to support HTML 5, with fallback to object or embed.

--- plugins/fabrik_element/fileupload/models/audio.php
+++ plugins/fabrik_element/fileupload/models/audio.php
@@ -57,7 +57,44 @@ class AudioRender
 	public function render(&$model, &$params, $file)
 	{
 		$file = str_replace("\\", "/", COM_FABRIK_LIVESITE . $file);
-		$this->output = "<embed src=\"$file\" autostart=\"false\" playcount=\"true\" loop=\"false\" height=\"50\" width=\"200\">";
+		//$this->output = "<embed src=\"$file\" autostart=\"false\" playcount=\"true\" loop=\"false\" height=\"50\" width=\"200\"  type=\"audio/mpeg\" />";
+		
+		/**
+		 * Theoretically this should make sure browsers that support HTML 5 use it, if no HTML5 then IE will use 'object'
+		 * and everything else will use 'embed'.  Trying to make sure nothing does autoplay, but FF is being difficult!
+		 */
+		
+		$this->output = '
+<audio src="' . $file . '"  controls>
+<!--[if !ie]> -->
+	<object data="' . $file . '"
+      type="audio/x-mpeg">
+		<param name="autoplay"
+       		value="false" />
+		<param name="width"
+       		value="140" />
+		<param name="height"
+       		value="40" />
+		<param name="controller"
+       		value="true" />
+		<param name="autostart"
+       		value="0" />
+		Oops!
+	</object>
+<!--<![endif]-->
+<!--[if ie]>	
+	<embed
+		src="' . $file . '"
+		autostart="false"
+		playcount="true"
+		loop="false"
+		height="50"
+		width="200"
+		type="audio/mpeg"
+	/>
+<![endif]-->
+</audio>
+		';
 	}
 
 	/**
