1e59b675fdb39e978759251e762232713116dd89
Hugh Messenger <hugh.messenger@gmail.com>
2015-02-06 15:52:13 -0600

    ixed issue where JS events weren't getting renewed on CDD change.

--- plugins/fabrik_element/cascadingdropdown/cascadingdropdown.js
+++ plugins/fabrik_element/cascadingdropdown/cascadingdropdown.js
@@ -181,6 +181,7 @@ var FbCascadingdropdown = new Class({
 					this.element.removeClass('readonly');
 				}
 			}
+			this.renewEvents();
 			// $$$ hugh - need to fire this CDD's 'change' event in case we have another CDD
 			// daisy chained on us.  We just don't need to do it if 'ignoreAjax' is true, because
 			// that means we're being added to the form, and everyone will get their change() method
