84c90d208888848966eb7d0f8ded94d3a01de446
pollen8 <rob@pollen-8.co.uk>
2015-03-03 11:34:02 +0100

    add missing button layout file

---
+++ plugins/fabrik_element/button/layouts/fabrik-element-button-form.php
@@ -0,0 +1,12 @@
+<?php
+// Add span with id so that element fxs work.
+$d = $displayData;
+
+?>
+
+<button class="<?php echo $d['class']; ?>" id="<?php echo $d['id']; ?>" name="<?php echo $d['name']; ?>">
+	<?php if ($d['icon'] !== '') : ?>
+		<span class="<?php echo $d['icon']; ?>"></span>
+	<?php endif; ?>
+	<?php echo $d['label']; ?>
+</button>
