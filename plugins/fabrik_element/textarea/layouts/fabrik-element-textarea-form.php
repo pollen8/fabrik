16ba21c532228db3a218cb97cfb850201a37b77e
pollen8 <rob@pollen-8.co.uk>
2015-04-03 00:25:13 +0200

    JLayouts. for textarea

---
+++ plugins/fabrik_element/textarea/layouts/fabrik-element-textarea-form.php
@@ -0,0 +1,17 @@
+<?php
+
+defined('JPATH_BASE') or die;
+
+// Add span with id so that element fxs work.
+$d = $displayData;
+
+?>
+<textarea
+	<?php foreach ($d['attributes'] as $key => $value) :
+	echo $key . '="' . $value . '"';
+endforeach;
+	?>><?php echo $d['value'];?></textarea>
+
+<?php if ($d['showCharsLeft']) : ?>
+	<?php echo $this->sublayout('charsleft', $d);?>
+<?php endif; ?>
