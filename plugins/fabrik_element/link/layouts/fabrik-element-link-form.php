dade7e0215cb7c819cb3298f7afb95d0f82efe46
pollen8 <rob@pollen-8.co.uk>
2015-04-03 15:26:56 +0200

    JLayouts. for link element

---
+++ plugins/fabrik_element/link/layouts/fabrik-element-link-form.php
@@ -0,0 +1,30 @@
+<?php
+defined('JPATH_BASE') or die;
+
+$d          = $displayData;
+
+$linkAttributes = array();
+$labelAttributes = array();
+
+foreach ($d->linkAttributes as $key => $val)
+{
+	$linkAttributes[] = $key . '="' . $val . '"';
+}
+
+$linkAttributes = implode("\n", $linkAttributes);
+
+
+foreach ($d->labelAttributes as $key => $val)
+{
+	$labelAttributes[] = $key . '="' . $val . '"';
+}
+
+$labelAttributes = implode("\n", $labelAttributes);
+
+?>
+
+<div class="fabrikSubElementContainer" id="<?php echo $d->id;?>">
+	<input type="text" <?php echo $linkAttributes;?> />
+	<input type="text" <?php echo $labelAttributes;?> />
+
+</div>
