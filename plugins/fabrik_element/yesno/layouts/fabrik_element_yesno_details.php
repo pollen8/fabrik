bee6c23e86a19b2f5e55f57147066f938a5f25c8
pollen8 <rob@pollen-8.co.uk>
2015-04-03 17:54:00 +0200

    convert yesno and youtube jlayouts to use object

--- plugins/fabrik_element/yesno/layouts/fabrik_element_yesno_details.php
+++ plugins/fabrik_element/yesno/layouts/fabrik_element_yesno_details.php
@@ -12,33 +12,27 @@
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
-$data = $displayData['value'];
-$tmpl = $displayData['tmpl'];
+$d = $displayData;
+$data = $d->value;
+$tmpl = $d->tmpl;
 $j3 = FabrikWorker::j3();
-
-$app = JFactory::getApplication();
-$format = $app->input->get('format', '');
 
 $opts = array();
 $properties = array();
 
-if ($format == 'pdf')
-{
+if ($d->format == 'pdf') :
 	$opts['forceImage'] = true;
 	FabrikHelperHTML::addPath(COM_FABRIK_BASE . 'plugins/fabrik_element/yesno/images/', 'image', 'list', false);
-}
+endif;
 
-if ($data == '1')
-{
+if ($data == '1') :
 	$icon = $j3 && $format != 'pdf' ? 'checkmark.png' : '1.png';
 	$properties['alt'] = FText::_('JYES');
 
 	echo FabrikHelperHTML::image($icon, 'list', $tmpl, $properties, false, $opts);
-}
-else
-{
+else :
 	$icon = $j3 && $format != 'pdf' ? 'remove.png' : '0.png';
 	$properties['alt'] = FText::_('JNO');
 	
 	echo FabrikHelperHTML::image($icon, 'list', $tmpl, $properties, false, $opts);
-}
+endif;
