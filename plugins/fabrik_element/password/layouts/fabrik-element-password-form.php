2a98f693cfa1cd8b25b64509f9d298d68ae74487
pollen8 <rob@pollen-8.co.uk>
2015-04-03 15:54:15 +0200

    JLayouts. for password element

---
+++ plugins/fabrik_element/password/layouts/fabrik-element-password-form.php
@@ -0,0 +1,43 @@
+<?php
+
+defined('JPATH_BASE') or die;
+
+$d = $displayData;
+
+$pw1Attributes = array();
+$pw2Attributes = array();
+
+foreach ($d->pw1Attributes as $key => $val)
+{
+	$pw1Attributes[] = $key . '="' . $val . '"';
+}
+
+$pw1Attributes = implode("\n", $pw1Attributes);
+
+
+foreach ($d->pw2Attributes as $key => $val)
+{
+	$pw2Attributes[] = $key . '="' . $val . '"';
+}
+
+$pw2Attributes = implode("\n", $pw2Attributes);
+
+
+?>
+<input type="password" <?php echo $pw1Attributes; ?>"  />
+
+<?php
+if ($d->showStrengthMeter) :
+	if ($d->j3) :
+		?>
+		<div class="strength progress progress-striped" style="margin-top:20px;width:40%;"></div>
+	<?php
+	else :
+		?>
+		<span class="strength"></span>
+	<?php
+	endif;
+endif;
+?>
+
+<input type="password" <?php echo $pw2Attributes; ?>"  />
