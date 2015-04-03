83a857161154052ca304bbc62d57e192b36e548c
pollen8 <rob@pollen-8.co.uk>
2015-04-03 15:00:43 +0200

    JLayouts. for image element

---
+++ plugins/fabrik_element/image/layouts/fabrik-element-image-form.php
@@ -0,0 +1,32 @@
+<?php
+defined('JPATH_BASE') or die;
+
+$d = $displayData;
+
+$image = '<img src="' . $d->defaultImage . '" alt="' . $d->value . '" ' . $d->float . ' class="imagedisplayor"/>';
+?>
+<div class="fabrikSubElementContainer" id="<?php echo $d->id; ?>">
+	<?php
+	if ($d->canSelect) :
+		?>
+		<img src="<?php echo $d->defaultImage; ?>" alt="<?php echo $d->value; ?>" <?php echo $d->float; ?> class="imagedisplayor" />
+		<br />
+		<?php echo JHTML::_('select.genericlist', $d->images, $d->imageName, 'class="inputbox imageselector" ', 'value', 'text', $d->image);
+		echo FabrikHelperHTML::folderAjaxSelect($d->folders);
+		?>
+		<input type="hidden" name="<?php echo $d->name; ?>" value="<?php echo $d->value; ?>" class="fabrikinput hiddenimagepath folderpath" />
+	<?php
+	else :
+		if ($d->linkURL) :
+			?>
+			<a href="<?php echo $d->linkURL; ?>" target="_blank"><?php echo $image; ?></a>
+		<?php
+		else :
+			echo $image;
+		endif;
+		?>
+		<input type="hidden" name="<?php echo $d->name; ?>" value="<?php echo $d->value; ?>" class="fabrikinput hiddenimagepath folderpath" />
+	<?php
+	endif;
+	?>
+</div>
