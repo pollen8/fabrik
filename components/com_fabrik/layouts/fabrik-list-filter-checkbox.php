4e0ab8204247a1c338c9b355349dc18fcc9e261e
pollen8 <rob@pollen-8.co.uk>
2015-04-28 17:14:44 +0200

    defaulting checkbox layout filter to use 1 row as before previous commit

--- components/com_fabrik/layouts/fabrik-list-filter-checkbox.php
+++ components/com_fabrik/layouts/fabrik-list-filter-checkbox.php
@@ -4,4 +4,4 @@ defined('JPATH_BASE') or die;
 $d    = $displayData;
 
 echo implode("\n", FabrikHelperHTML::grid($d->values, $d->labels, $d->default, $d->name,
-	'checkbox', false, 2, array('input' => array('fabrik_filter'))));
+	'checkbox', false, 1, array('input' => array('fabrik_filter'))));
