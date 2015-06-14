f9ee66b20877f32c9acc4544a18ad0ade5bf7298
Hugh Messenger <hugh.messenger@gmail.com>
2015-06-14 12:36:40 -0500

    Removing some un-needed code from example custom CSS.

--- components/com_fabrik/views/list/tmpl/bootstrap/custom_css_example.php
+++ components/com_fabrik/views/list/tmpl/bootstrap/custom_css_example.php
@@ -35,8 +35,6 @@
 
 header('Content-type: text/css');
 $c = $_REQUEST['c'];
-$buttonCount = (int) $_REQUEST['buttoncount'];
-$buttonTotal = $buttonCount === 0 ? '100%' : 30 * $buttonCount ."px";
 echo "
 
 #listform_$c .fabrikForm {
