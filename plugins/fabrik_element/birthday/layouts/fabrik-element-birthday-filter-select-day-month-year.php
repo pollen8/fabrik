67d32f9444591889caffb76dd31290ede597b2ca
pollen8 <rob@pollen-8.co.uk>
2015-06-15 18:08:01 +0200

    fixed birthday filter had wrong classes. Meant values were being ignored when exporting to csv

--- plugins/fabrik_element/birthday/layouts/fabrik-element-birthday-filter-select-day-month-year.php
+++ plugins/fabrik_element/birthday/layouts/fabrik-element-birthday-filter-select-day-month-year.php
@@ -7,7 +7,7 @@ $d = $displayData;
 // Order of the selects is important - do not change
 ?>
 
-<select name="<?php echo $d->name . '[year]';?>" class="input-small fabrikinput">
+<select name="<?php echo $d->name . '[year]';?>" class="input-small fabrik_filter">
 	<?php foreach ($d->years as $item) :
 		$selected = ArrayHelper::getValue($d->default, 0) == $item->value ? 'selected' : ''?>
 		<option value="<?php echo $item->value;?>" <?php echo $selected;?>>
@@ -16,7 +16,7 @@ $d = $displayData;
 	<?php endforeach; ?>
 </select>
 
-<select name="<?php echo $d->name . '[month]';?>" class="input-small fabrikinput">
+<select name="<?php echo $d->name . '[month]';?>" class="input-small fabrik_filter">
 	<?php foreach ($d->months as $item) :
 		$selected =  ArrayHelper::getValue($d->default, 1) == $item->value ? 'selected' : ''?>
 		<option value="<?php echo $item->value;?>" <?php echo $selected;?>>
@@ -25,7 +25,7 @@ $d = $displayData;
 	<?php endforeach; ?>
 </select>
 
-<select name="<?php echo $d->name . '[day]';?>" class="input-small fabrikinput">
+<select name="<?php echo $d->name . '[day]';?>" class="input-small fabrik_filter">
 	<?php foreach ($d->days as $item) :
 		$selected =  ArrayHelper::getValue($d->default, 2) == $item->value ? 'selected' : ''?>
 		<option value="<?php echo $item->value;?>" <?php echo $selected;?>>
