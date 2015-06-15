0361bcafcdac2225cac3260c833875c2aaed6f57
pollen8 <rob@pollen-8.co.uk>
2015-06-15 12:42:08 +0200

    notices on birthday filter

--- plugins/fabrik_element/birthday/layouts/fabrik-element-birthday-filter-select-day-month-year.php
+++ plugins/fabrik_element/birthday/layouts/fabrik-element-birthday-filter-select-day-month-year.php
@@ -1,6 +1,7 @@
 <?php
 defined('JPATH_BASE') or die;
 
+use Joomla\Utilities\ArrayHelper;
 $d = $displayData;
 
 // Order of the selects is important - do not change
@@ -8,7 +9,7 @@ $d = $displayData;
 
 <select name="<?php echo $d->name . '[year]';?>" class="input-small fabrikinput">
 	<?php foreach ($d->years as $item) :
-		$selected = $d->default[0] == $item->value ? 'selected' : ''?>
+		$selected = ArrayHelper::getValue($d->default, 0) == $item->value ? 'selected' : ''?>
 		<option value="<?php echo $item->value;?>" <?php echo $selected;?>>
 			<?php echo $item->text;?>
 		</option>
@@ -17,7 +18,7 @@ $d = $displayData;
 
 <select name="<?php echo $d->name . '[month]';?>" class="input-small fabrikinput">
 	<?php foreach ($d->months as $item) :
-		$selected = $d->default[1] == $item->value ? 'selected' : ''?>
+		$selected =  ArrayHelper::getValue($d->default, 1) == $item->value ? 'selected' : ''?>
 		<option value="<?php echo $item->value;?>" <?php echo $selected;?>>
 			<?php echo $item->text;?>
 		</option>
@@ -26,7 +27,7 @@ $d = $displayData;
 
 <select name="<?php echo $d->name . '[day]';?>" class="input-small fabrikinput">
 	<?php foreach ($d->days as $item) :
-		$selected = $d->default[2] == $item->value ? 'selected' : ''?>
+		$selected =  ArrayHelper::getValue($d->default, 2) == $item->value ? 'selected' : ''?>
 		<option value="<?php echo $item->value;?>" <?php echo $selected;?>>
 			<?php echo $item->text;?>
 		</option>
