b02eec9317174170fc31397b2f7c7da94f14d3b0
pollen8 <rob@pollen-8.co.uk>
2015-12-11 10:57:48 +0100

    adding missing js files

---
+++ administrator/components/com_fabrik/models/fields/fabrikcontenttypelist.js
@@ -0,0 +1,35 @@
+/**
+ * Content Type Ajax Preview
+ *
+ * @copyright: Copyright (C) 2005-2015, fabrikar.com - All rights reserved.
+ * @license:   GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+'use strict';
+
+var FabrikContentTypeList = new Class({
+
+    options: {},
+
+    initialize: function (id) {
+        var showUpdate = this.showUpdate;
+        showUpdate(jQuery('#' + id).val());
+        jQuery('#' + id).on('change', function () {
+            showUpdate(jQuery(this).val());
+        });
+    },
+
+    showUpdate: function (contentType) {
+        jQuery.ajax({
+            url: 'index.php',
+            data: {
+                option: 'com_fabrik',
+                task: 'contenttype.preview',
+                contentType: contentType
+            }
+        }).done(function (data) {
+            jQuery('#contentTypeListPreview').empty().html(data);
+        });
+    }
+
+});
