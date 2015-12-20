1ae7ab80d71721e5b943547fcf3b3f4a213dbf19
skurvish <achartier@admiralsecure.com>
2015-12-20 09:48:22 -0500

    [FIX] Corrected missing disable option.

--- plugins/fabrik_element/dropdown/layouts/fabrik-element-dropdown-form.php
+++ plugins/fabrik_element/dropdown/layouts/fabrik-element-dropdown-form.php
@@ -9,7 +9,8 @@ $multisize = $d->multisize === '' ? '' : 'size="' . $d->multisize . '""';
 <select name="<?php echo $d->name ?>" id="<?php echo $d->id ?>" <?php echo $multiple; ?>
 	<?php echo $multisize; ?> <?php echo $d->attribs; ?>>
 	<?php foreach ($d->options as $opt) :
+		$disabled = $opt->disable === true ? ' disabled' : ''; 
 		$selected = in_array($opt->value, $d->selected) ? ' selected="selected" ' : ''; ?>
-		<option value="<?php echo $opt->value; ?>" <?php echo $selected; ?>><?php echo JText::_($opt->text); ?></option>
+		<option value="<?php echo $opt->value;?>" <?php  echo $disabled; ?><?php echo $selected; ?>><?php echo JText::_($opt->text); ?></option>
 	<?php endforeach; ?>
 </select>
