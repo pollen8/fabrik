8a856f83c44182edd04ccfa2fcbe05791918545d
Hugh Messenger <hugh.messenger@gmail.com>
2015-12-28 06:56:00 -0600

    Adding fullcalendar layouts

---
+++ plugins/fabrik_visualization/fullcalendar/layouts/fabrik-visualization-fullcalendar-event-popup.php
@@ -0,0 +1,16 @@
+<?php
+defined('JPATH_BASE') or die;
+?>
+<a
+	id=""
+	class="fabrikEvent label"
+	href="#"
+	data-toggle="popover"
+	data-placement="top"
+	data-title=""
+	data-content=""
+	data-trigger="click"
+	data-html="true"
+	data-container="body"
+>
+</a>
