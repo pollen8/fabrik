e679fbc82f0316c467adaea70b781c01077e6eda
pollen8 <rob@pollen-8.co.uk>
2015-12-28 16:15:16 +0100

    woopsy in last system plugin fix, js was not loading

--- plugins/system/fabrik/fabrik.php
+++ plugins/system/fabrik/fabrik.php
@@ -122,10 +122,10 @@ class PlgSystemFabrik extends JPlugin
 	public static function buildJs()
 	{
 		$session = JFactory::getSession();
-		$config  = (array) $session->get('fabrik.js.config', array(), 'array');
+		$config  = (array) $session->get('fabrik.js.config', array());
 		$config  = implode("\n", $config);
 
-		$js = (array) $session->get('fabrik.js.scripts', array(), 'array');
+		$js = (array) $session->get('fabrik.js.scripts', array());
 		$js = implode("\n", $js);
 
 		if ($config . $js !== '')
