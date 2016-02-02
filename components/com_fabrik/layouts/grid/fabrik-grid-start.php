94b09548bcf0e849952ed7d8b80a69f57f988823
pollen8 <rob@pollen-8.co.uk>
2016-02-02 19:41:53 +0100

    changed: form group layouts now use form.fabrik-control-group jlayout overrides - means we can fix group column properites for bs3 etc

---
+++ components/com_fabrik/layouts/grid/fabrik-grid-start.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Grid start
+ *
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @copyright   Copyright (C) 2005-2015 fabrikar.com - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ * @since       3.4
+ */
+?>
+<div class="row-fluid">
