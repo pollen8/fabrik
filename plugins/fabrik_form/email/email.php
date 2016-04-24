21bdc97467db7d7fe236694386bf6091ee618ed4
Rob Clayvurn <rob@pollen-8.co.uk>
2016-04-24 11:45:17 +0200

    fixes GH-1685, uft8 decode of pdf content in email plugin

--- plugins/fabrik_form/email/email.php
+++ plugins/fabrik_form/email/email.php
@@ -467,7 +467,7 @@ class PlgFabrik_FormEmail extends PlgFabrik_Form
 			}
 
 			// Load the HTML into DOMPdf and render it.
-			$domPdf->load_html($html);
+			$domPdf->load_html(utf8_decode($html));
 			$domPdf->render();
 
 			// Store the file in the tmp folder so it can be attached
