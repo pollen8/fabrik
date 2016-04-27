c7980c23070958ed688313d67f7e10eeb13caa3f
Hugh Messenger <hugh.messenger@gmail.com>
2016-04-27 12:40:02 -0500

    Fixing Caneditrow naming

--- plugins/fabrik_list/caneditrow/caneditrow.php
+++ plugins/fabrik_list/caneditrow/caneditrow.php
@@ -158,7 +158,7 @@ class PlgFabrik_ListCaneditrow extends PlgFabrik_List
 		$opts = $this->getElementJSOptions();
 		$opts->acl = $this->acl;
 		$opts = json_encode($opts);
-		$this->jsInstance = "new FbListCanEditRow($opts)";
+		$this->jsInstance = "new FbListcaneditrow($opts)";
 
 		return true;
 	}
@@ -170,6 +170,6 @@ class PlgFabrik_ListCaneditrow extends PlgFabrik_List
 	 */
 	public function loadJavascriptClassName_result()
 	{
-		return 'FbListCanEditRow';
+		return 'FbListcaneditrow';
 	}
 }
