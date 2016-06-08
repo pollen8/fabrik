2dbf27767f6fc668e3040726a02080d745ac698b
Hugh Messenger <hugh.messenger@gmail.com>
2016-06-08 15:40:44 -0500

    Changed from parseMessageForRowholder() to parseMessageForPlaceHolder() in calc list render.  I think the former is deprecated, see comment on the function.  Used to be needed years ago, to acocunt for differences in data format between list and form display.

--- plugins/fabrik_element/calc/calc.php
+++ plugins/fabrik_element/calc/calc.php
@@ -358,7 +358,8 @@ class PlgFabrik_ElementCalc extends PlgFabrik_Element
 			//  $$$ Paul - Because this is run on List rows before repeat-group merges, repeat group placeholders are OK.
 			//  $$$ hugh - standardizing on $data but need need $d here for backward compat
 			$d = $data;
-			$cal = $listModel->parseMessageForRowHolder($cal, $data, true);
+			$w = new FabrikWorker;
+			$cal = $w->parseMessageForPlaceHolder($cal, $data, true, true);
 
 			if (FabrikHelperHTML::isDebug())
 			{
