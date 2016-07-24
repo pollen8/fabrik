8f0e1ed3c6bd25da4c4065b2295f9d2dee614f83
troester <trob@users.noreply.github.com>
2016-07-24 12:15:21 +0200

    Create fabrikfolderlist.php

---
+++ administrator/components/com_fabrik/models/fields/fabrikfolderlist.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Get a list of templates - either in components/com_fabrik/views/{view}/tmpl or {view}/tmpl25
+ *
+ * @package     Joomla
+ * @subpackage  Form
+ * @copyright   Copyright (C) 2005-2015 fabrikar.com - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.html.html');
+jimport('joomla.form.formfield');
+jimport('joomla.form.helper');
+JFormHelper::loadFieldClass('folderlist');
+
+/**
+ * Get a list of templates - either in components/com_fabrik/views/{view}/tmpl or {view}/tmpl25
+ *
+ * @package     Joomla
+ * @subpackage  Form
+ * @since       3.1b
+ */
+class JFormFieldFabrikFolderlist extends JFormFieldFolderList
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var		string
+	 * @since	1.6
+	 */
+	public $type = 'FabrikFolderlist';
+
+	/**
+	 * Method to get the field options.
+	 *
+	 * @return  array  The field option objects.
+	 */
+	protected function getOptions()
+	{
+		$dir = JPATH_ROOT . $this->element['directory'];
+
+		$dir = str_replace('\\', '/', $dir);
+		$dir = str_replace('//', '/', $dir);
+
+
+		$this->element['directory'] = $this->directory = $dir;
+
+
+		$opts = parent::getOptions();
+
+		foreach ($opts as &$opt)
+		{
+			$opt->value = str_replace('\\', '/', $opt->value);
+			$opt->value = str_replace('//', '/', $opt->value);
+			$opt->value = str_replace($dir, '', $opt->value);
+			$opt->text = str_replace('\\', '/', $opt->text);
+			$opt->text = str_replace('//', '/', $opt->text);
+			$opt->text = str_replace($dir, '', $opt->text);
+		}
+
+		return $opts;
+	}
+}
