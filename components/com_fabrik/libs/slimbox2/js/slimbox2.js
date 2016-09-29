be40f1c5485b6daba440fcb729c1faca4d2be8ce
Hugh Messenger <hugh.messenger@gmail.com>
2016-09-29 17:18:12 -0500

    Workaround for issue with scanPage ... if list loaded without lightbox, then AJAX nav, it'll try and rescan.

--- components/com_fabrik/libs/slimbox2/js/slimbox2.js
+++ components/com_fabrik/libs/slimbox2/js/slimbox2.js
@@ -9,11 +9,13 @@
 // AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
 jQuery.slimbox.scanPage = function() {
 	if (!/series60|symbian|windows ce|blackberry/i.test(navigator.userAgent)) {
-		jQuery(function($) {
-			$("a[rel^='lightbox']").slimbox({/* Put custom options here */}, null, function(el) {
-				return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
+		if (typeof(Lightbox) !== 'undefined') {
+			jQuery(function ($) {
+				$("a[rel^='lightbox']").slimbox({/* Put custom options here */}, null, function (el) {
+					return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
+				});
 			});
-		});
+		}
 	}
 };
 
