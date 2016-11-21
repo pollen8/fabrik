2d67cef623a85a2c4f7d482a59e26e9c47d9a5e8
Hugh Messenger <hugh.messenger@gmail.com>
2016-11-21 15:26:42 -0600

    Fixes for AJAX updateRows with grouping

--- media/com_fabrik/js/list.js
+++ media/com_fabrik/js/list.js
@@ -506,7 +506,7 @@ define(['jquery', 'fab/fabrik', 'fab/list-toggle', 'fab/list-grouped-toggler', '
                             if (res.count < res.total) {
                                 self.triggerCSVExport(res.count);
                             } else {
-                                var finalurl = 'index.php?option=com_fabrik&view=list&format=csv&listid=' + self.id +
+                                var finalurl = Fabrik.liveSite + 'index.php?option=com_fabrik&view=list&format=csv&listid=' + self.id +
                                     '&start=' + res.count + '&Itemid=' + self.options.Itemid;
                                 var msg = '<div class="alert alert-success" style="padding:10px;margin-bottom:3px"><h3>' + Joomla.JText._('COM_FABRIK_CSV_COMPLETE');
                                 msg += '</h3><p><a class="btn btn-success" href="' + finalurl + '">' +
@@ -1101,13 +1101,17 @@ define(['jquery', 'fab/fabrik', 'fab/list-toggle', 'fab/list-grouped-toggler', '
                         'task'    : 'list.view',
                         'format'  : 'raw',
                         'listid'  : this.id,
-                        'group_by': this.options.groupedBy,
                         'listref' : this.options.listRef
                     };
                 data['limit' + this.id] = this.options.limitLength;
 
                 if (extraData) {
                     Object.append(data, extraData);
+                }
+
+                if (this.options.groupedBy !== '')
+                {
+                    data['grouped_by'] = this.options.groupedBy;
                 }
 
                 new Request({
@@ -1230,7 +1234,9 @@ define(['jquery', 'fab/fabrik', 'fab/list-toggle', 'fab/list-grouped-toggler', '
                 // $$$ rob was $H(data.data) but that wasnt working ????
                 // testing with $H back in again for grouped by data? Yeah works for
                 // grouped data!!
-                var gdata = this.options.isGrouped || this.options.groupedBy !== '' ? $H(data.data) : data.data;
+                // $$$ hugh back out again!!
+                //var gdata = this.options.isGrouped || this.options.groupedBy !== '' ? $H(data.data) : data.data;
+                var gdata = data.data;
                 var gcounter = 0;
                 gdata.each(function (groupData, groupKey) {
                     tbody = self.options.isGrouped ? self.list.getElements('.fabrik_groupdata')[gcounter] : self.tbody;
