67d67c05bac8f00eadb49fd5e173bbcba352139f
troester <trob@users.noreply.github.com>
2016-12-11 11:49:44 +0100

    Fix for typo (thx startpoint)

--- plugins/fabrik_element/youtube/layouts/fabrik-element-youtube-detail.php
+++ plugins/fabrik_element/youtube/layouts/fabrik-element-youtube-detail.php
@@ -13,6 +13,6 @@ $scheme = $uri->getScheme();
 	type="text/html"
 	width="<?php echo $d->width;?>"
 	height="<?php echo $d->height; ?>"
-    src=<?php echo $scheme; ?>"://www.youtube.com/embed/<?php echo $d->vid; ?>?autoplay=<?php echo $d->autoplay; ?>&fs=<?php echo $d->fs; ?>"
+    src="<?php echo $scheme; ?>://www.youtube.com/embed/<?php echo $d->vid; ?>?autoplay=<?php echo $d->autoplay; ?>&fs=<?php echo $d->fs; ?>"
     frameborder="0"
 ></iframe>
