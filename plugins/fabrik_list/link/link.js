80e65699180277c44aff917f7d2d5a92aefa0670
Hugh Messenger <hugh.messenger@gmail.com>
2016-12-24 13:57:39 -0600

    First cut at 'link' list plugin.  Allows a button that opens a custom link (with placeholders), optionally designated as a Fabrik link which can open as "AJAXified" in a popup.  Kind of like setting an element's custom detail link, but adds a button for it.

---
+++ plugins/fabrik_list/link/link.js
@@ -0,0 +1,76 @@
+/**
+ * List JS
+ *
+ * @copyright: Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license:   GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+define(['jquery', 'fab/list-plugin'], function (jQuery, FbListPlugin) {
+	var FbListLink = new Class({
+		Extends: FbListPlugin,
+
+		options: {
+			'link': '',
+			'fabrikLink': false,
+			'windowTitle': ''
+		},
+
+		initialize: function (options) {
+			this.parent(options);
+		},
+
+		buttonAction: function () {
+			var statusMsg;
+			var chxs = this.list.getForm().getElements('input[name^=ids]').filter(function (i) {
+				return i.checked;
+			});
+
+			var ids = chxs.map(function (chx) {
+				return chx.get('value');
+			});
+
+			// Build rows object for ease of access to selected rows' data
+			var rows = {};
+			chxs.each(function (chx) {
+				var id = chx.get('value');
+				rows[id] = this.list.getRow(id);
+			}.bind(this));
+
+			if (this.options.link !== '') {
+				var thisLink = this.options.link;
+				jQuery.each(rows[ids[0]], function (key, value){
+					if (key === '__pk_val') {
+						key = 'rowid';
+					}
+					var re = new RegExp('\{' + key + '\}');
+					thisLink = thisLink.replace(re, value);
+				});
+				if (this.options.fabrikLink && this.list.options.ajax_links) {
+					var loadMethod = 'xhr';
+					thisLink += thisLink.contains('?') ? '&' : '?';
+					thisLink += 'tmpl=component&ajax=1';
+					thisLink += '&format=partial';
+					var winOpts = {
+						'id'        : 'add.' + this.list.id,
+						'title'     : this.options.windowTitle,
+						'loadMethod': loadMethod,
+						'contentURL': thisLink,
+						'width'     : this.list.options.popup_width,
+						'height'    : this.list.options.popup_height
+					};
+					if (this.list.options.popup_offset_x !== null) {
+						winOpts.offset_x = this.list.options.popup_offset_x;
+					}
+					if (this.list.options.popup_offset_y !== null) {
+						winOpts.offset_y = this.options.popup_offset_y;
+					}
+					Fabrik.getWindow(winOpts);
+				}
+				else {
+					window.location = thisLink;
+				}
+			}
+		}
+	});
+
+	return FbListLink;
+});
---
+++ plugins/fabrik_list/link/link.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ *  Add an action button to run PHP
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Fabrik.list.php
+ * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+// Require the abstract plugin class
+require_once COM_FABRIK_FRONTEND . '/models/plugin-list.php';
+
+/**
+ *  Add an action button to run PHP
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Fabrik.list.php
+ * @since       3.0
+ */
+
+class PlgFabrik_ListLink extends plgFabrik_List
+{
+	protected $buttonPrefix = 'link';
+
+	protected $msg = null;
+
+	/**
+	 * Prep the button if needed
+	 *
+	 * @param   array  &$args  Arguments
+	 *
+	 * @return  bool;
+	 */
+
+	public function button(&$args)
+	{
+		parent::button($args);
+
+		return true;
+	}
+
+	/**
+	 * Get button image
+	 *
+	 * @since   3.1b
+	 *
+	 * @return   string  image
+	 */
+
+	protected function getImageName()
+	{
+		$img = parent::getImageName();
+
+		return $img;
+	}
+
+	/**
+	 * Get the button label
+	 *
+	 * @return  string
+	 */
+
+	protected function buttonLabel()
+	{
+		return $this->getParams()->get('table_php_button_label', parent::buttonLabel());
+	}
+
+	/**
+	 * Get the parameter name that defines the plugins acl access
+	 *
+	 * @return  string
+	 */
+
+	protected function getAclParam()
+	{
+		return 'table_link_access';
+	}
+
+	/**
+	 * Can the plug-in select list rows
+	 *
+	 * @return  bool
+	 */
+
+	public function canSelectRows()
+	{
+		return true;
+	}
+
+	/**
+	 * Return the javascript to create an instance of the class defined in formJavascriptClass
+	 *
+	 * @param   array  $args  Array [0] => string table's form id to contain plugin
+	 *
+	 * @return bool
+	 */
+
+	public function onLoadJavascriptInstance($args)
+	{
+		parent::onLoadJavascriptInstance($args);
+		$opts = $this->getElementJSOptions();
+		$params = $this->getParams();
+		$opts->link = $params->get('table_link_link', '');
+		$opts->fabrikLink = $params->get('table_link_isfabrik', '0') === '1';
+		$opts->windowTitle = $params->get('table_link_fabrik_window_title', '');
+		$opts = json_encode($opts);
+		$this->jsInstance = "new FbListLink($opts)";
+
+		return true;
+	}
+
+	/**
+	 * Load the AMD module class name
+	 *
+	 * @return string
+	 */
+	public function loadJavascriptClassName_result()
+	{
+		return 'FbListLink';
+	}
+
+}
