bd96febb5dcdc6fff13d87bbf165e6b20c868516
Hugh Messenger <hugh.messenger@gmail.com>
2017-01-17 18:27:40 -0600

    Remove prepended Fabrik.liveSite on CSV export download url, so exporting on the backend doesn't try and download through the front end.  This change wasn't supposed to get committed anyway, it crept in while testing something a few months ago.

--- media/com_fabrik/js/list.js
+++ media/com_fabrik/js/list.js
@@ -508,7 +508,7 @@ define(['jquery', 'fab/fabrik', 'fab/list-toggle', 'fab/list-grouped-toggler', '
                             if (res.count < res.total) {
                                 self.triggerCSVExport(res.count);
                             } else {
-                                var finalurl = Fabrik.liveSite + 'index.php?option=com_fabrik&view=list&format=csv&listid=' + self.id +
+                                var finalurl = 'index.php?option=com_fabrik&view=list&format=csv&listid=' + self.id +
                                     '&start=' + res.count + '&Itemid=' + self.options.Itemid;
                                 var msg = '<div class="alert alert-success" style="padding:10px;margin-bottom:3px"><h3>' + Joomla.JText._('COM_FABRIK_CSV_COMPLETE');
                                 msg += '</h3><p><a class="btn btn-success" href="' + finalurl + '">' +
