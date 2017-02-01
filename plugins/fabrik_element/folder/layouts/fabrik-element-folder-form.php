0d3a7223c5a3866f821a4e3edef2e23ea9e070f3
Hugh Messenger <hugh.messenger@gmail.com>
2017-01-31 20:20:31 -0600

    Fixes for folder element.

--- plugins/fabrik_element/folder/layouts/fabrik-element-folder-form.php
+++ plugins/fabrik_element/folder/layouts/fabrik-element-folder-form.php
@@ -5,8 +5,9 @@ $d = $displayData;
 
 ?>
 <select id="<?php echo $d->id?>" class="fabrikinput inputbox <?php echo $d->errorCss;?>" name="<?php echo $d->name;?>">
-	<?php foreach ($d->options as $option) : ?>
-		<option value="<?php echo $option->value;?>">
+	<?php foreach ($d->options as $option) :
+		$selected = $option->value === $d->selected ? ' selected="selected" ' : ''; ?>
+		<option value="<?php echo $option->value;?>" <?php echo $selected; ?>>
 			<?php echo $option->text;?>
 		</option>
 	<?php endforeach; ?>
