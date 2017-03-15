c227914a2c783e262fb71600de81b30b667f2885
Hugh Messenger <hugh.messenger@gmail.com>
2017-03-15 15:22:23 -0500

    Minifying JS.

--- plugins/fabrik_list/webservice/webservice-min.js
+++ plugins/fabrik_list/webservice/webservice-min.js
@@ -1,2 +1,2 @@
 /*! Fabrik */
-define(["jquery","fab/list-plugin"],function(a,b){new Class({Extends:b,initialize:function(a){this.parent(a)},buttonAction:function(){this.list.submit("list.doPlugin")}})});
+define(["jquery","fab/list-plugin"],function(a,b){var c=new Class({Extends:b,initialize:function(a){this.parent(a)},buttonAction:function(){this.list.submit("list.doPlugin")}});return c});
