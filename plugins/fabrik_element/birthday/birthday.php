a87e497a0618725534dfea2ceb392e88a27c69f0
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2017-03-28 11:46:00 +0300

    updated comment

--- plugins/fabrik_element/birthday/birthday.php
+++ plugins/fabrik_element/birthday/birthday.php
@@ -368,8 +368,8 @@ class PlgFabrik_ElementBirthday extends PlgFabrik_Element
 	 * @TODO: if NULL value is the first in repeated group then in list view whole group is empty.
 	 * Could anyone find a solution? I give up :-(
 	 * Paul 20130904 I fixed the id fields and I am getting a string passed in as $val here yyyy-m-d.
-	 * Jaanus: $save could be date or nothing (null). Previous return '' wrote always '0000-00-00' as DATE field doesn't know ''. 
-	 * If an empty element is set to save null it should return null. 
+	 * Jaanus: saved data could be date or nothing (null). Previous return '' wrote always '0000-00-00' as DATE field doesn't know ''. 
+	 * such value as '' and therefore setting element to save null hadn't expected impact. Simple return; returns null as it should. 
 	 *
 	 * @return  null or string	yyyy-mm-dd 
 	 */
