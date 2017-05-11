0ac40c5cde35174bf59366cd20a285d3f7efdb91
Hugh Messenger <hugh.messenger@gmail.com>
2017-05-11 16:37:41 -0500

    Added fabrik-custom-button layout (does nothing, just there as a placeholder for overrides)

---
+++ components/com_fabrik/views/form/tmpl/bootstrap/layouts/form/fabrik-custom-button.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Do-nothing layout, which can be overridden to create custom button(s) that will be rendered
+ * next to the Submit button on a form.  Just a convenience so custom buttons can be added without
+ * using a custom template.
+ *
+ * $displayData is:
+ *
+ * 'formModel' => $model,
+ * 'row' => $row,
+ * 'rowid' => $thisRowId,
+ * 'itemid' => $itemId
+ *
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ * @since       3.6
+ */
+defined('JPATH_BASE') or die;
+$d = $displayData;
+?>
+
+<?php
+if ($d->formModel->getId() == 29) {
+	?>
+<button type="submit" class="btn btn-primary button save" name="Submit Foo">
+	Save Foo</button>
+<?php
+}
+?>
