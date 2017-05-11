3cc6db50dc7acc36a58c92f6edbb737937ebf1af
Hugh Messenger <hugh.messenger@gmail.com>
2017-05-11 03:32:25 -0500

    Adding custom button layout

---
+++ components/com_fabrik/layouts/form/fabrik-custom-button.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Do-nothing layout, which can be overridden to create custom button(s) that will be rendered
+ * next to the Submit button on a form.  Just a convenience so custom buttons can be added without
+ * using a custom template.
+ *
+ * $displayData is:
+ *
+ * 'formModel' => $model,
+ * 'row' => $row,
+ * 'rowid' => $thisRowId,
+ * 'itemid' => $itemId
+ *
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ * @since       3.6
+ */
+defined('JPATH_BASE') or die;
+$d = $displayData;
+?>
