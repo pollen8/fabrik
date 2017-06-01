5a0f268bf21dec4944ec4666be469e5ac2370efe
Hugh Messenger <hugh.messenger@gmail.com>
2017-06-01 15:26:54 -0500

    Simplified the nocaptcha layout, so it doesn't use a callback (had a site where async timing issues caused issues with callback).

--- plugins/fabrik_element/captcha/layouts/fabrik-element-captcha-nocaptcha.php
+++ plugins/fabrik_element/captcha/layouts/fabrik-element-captcha-nocaptcha.php
@@ -4,15 +4,6 @@ defined('JPATH_BASE') or die;
 $d = $displayData;
 ?>
 
-<script>
-var onloadCallback = function() {
-	grecaptcha.render('<?php echo $d->id; ?>', {
-		'sitekey' : '<?php echo $d->site_key; ?>'
-	});
-};
-</script>
+<div class ="g-recaptcha" data-sitekey="<?php echo $d->site_key; ?>"></div>
 
-<div class="captcha_input" id="<?php echo $d->id; ?>">
-</div>
-
-<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit&hl=<?php echo $d->lang; ?>" async defer></script>
+<script src="https://www.google.com/recaptcha/api.js?hl=<?php echo $d->lang; ?>" async defer></script>
