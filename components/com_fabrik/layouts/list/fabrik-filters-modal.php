299337c62b29983750a2646aeaf3974254c35e38
troester <trob@users.noreply.github.com>
2017-07-06 00:22:18 +0200

    oops

--- components/com_fabrik/layouts/list/fabrik-filters-modal.php
+++ components/com_fabrik/layouts/list/fabrik-filters-modal.php
@@ -66,7 +66,7 @@ endforeach;
 		</table>
 	</div>
 	<div class="modal-footer">
-		<a href="#" class="btn" data-dismiss="modal"><?php echo FabrikHelperHTML::icon('icon-cancel', FText::_('COM_FABRIK_CLEAR')); ?></a>
+		<a href="#" class="btn" data-dismiss="modal"><?php echo FabrikHelperHTML::icon('icon-cancel', FText::_('COM_FABRIK_CLOSE_WINDOW')); ?></a>
 		<?php
 		if ($d->showClearFilters) : ?>
 			<a class="btn clearFilters" href="#">
