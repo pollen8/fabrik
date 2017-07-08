cc728a280ecba06f7ed0193e71297b56c190f65b
troester <trob@users.noreply.github.com>
2017-07-08 10:02:36 +0200

    adding gridspan helper

--- components/com_fabrik/views/form/tmpl/bootstrap_tabs/default_group_details.php
+++ components/com_fabrik/views/form/tmpl/bootstrap_tabs/default_group_details.php
@@ -21,8 +21,8 @@ foreach ($this->elements as $element) :
 	if (!$element->hidden) :?>
 		<div class="<?php echo $element->span;?>">
 			<div class="row-fluid">
-				<div class="span4"><em><?php echo $element->label_raw ?></em></div>
-				<div class="span8"><?php echo $element->element;?></div>
+				<div class="<?php echo FabrikHelperHTML::getGridSpan(4); ?>"><em><?php echo $element->label_raw ?></em></div>
+				<div class="<?php echo FabrikHelperHTML::getGridSpan(8); ?>"><?php echo $element->element;?></div>
 			</div>
 		</div>
 
