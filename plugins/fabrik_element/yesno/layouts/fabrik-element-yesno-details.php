5f900e81385027817e0a4f852c65a42ffb6f2d82
Phil Bauer <pjbauer@ec.rr.com>
2017-07-18 22:53:28 -0400

    Update fabrik-element-yesno-details.php
    In addition to new per-element custom labeling for 'Yes' and 'No'
    Adding option to use specific label text (rather than any icon or image) in list and details view.
    Adds logic for assuming whether entered 'file name' is an icon- Icomoon suffix for styling , or an actual image file.

--- plugins/fabrik_element/yesno/layouts/fabrik-element-yesno-details.php
+++ plugins/fabrik_element/yesno/layouts/fabrik-element-yesno-details.php
@@ -20,25 +20,30 @@ $format = $d->format;
 $j3 = FabrikWorker::j3();
 
 $opts = array();
-$properties = array();
+$yes_label = (empty($d->yes_label)) ? FText::_('JYES') : FText::_($d->yes_label) ;
+$no_label = (empty($d->no_label)) ? FText::_('JNO') : FText::_($d->no_label) ;
+$show_label = (empty($d->show_label)) ? '0' : $d->show_label;
+$yes_image = (empty($d->yes_image)) ? 'checkmark' : $d->yes_image ;
+$no_image = (empty($d->no_image)) ? 'remove' : $d->no_image ;
 
-if ($d->format == 'pdf') :
+// if period . in image name assume it is a file
+if ($d->format == 'pdf' || strpos($yes_image,'.')>0 ) :
 	$opts['forceImage'] = true;
 	FabrikHelperHTML::addPath(COM_FABRIK_BASE . 'plugins/fabrik_element/yesno/images/', 'image', 'list', false);
 endif;
 
-$yes_image = ($d->yes_image == '') ? 'checkmark.png' : $d->yes_image ;
-$no_image = ($d->no_image == '') ? 'remove.png' : $d->no_image ;
 if(!empty($d->image_height)) $properties['style'] = FText::_('height:'.$d->image_height.'px!important');
-
+ 
 if ($data == '1') :
 	$icon = $j3 && $format != 'pdf' ? $yes_image : '1.png';
-	$properties['alt'] = FText::_('JYES');
-
-	echo FabrikHelperHTML::image($icon, 'list', $tmpl, $properties, false, $opts);
+	$properties['alt'] = $yes_label;
 else :
-	$icon = $j3 && $format != 'pdf' ? $no_image : '0.png'; 
-	$properties['alt'] = FText::_('JNO');
-
-	echo FabrikHelperHTML::image($icon, 'list', $tmpl, $properties, false, $opts);
+	$icon = $j3 && $format != 'pdf' ? $no_image : '0.png';
+    $properties['alt'] = $no_label;
 endif;
+//error_log('fabrik-element-yesno-details.php - '.FabrikHelperHTML::image($icon, 'details', $tmpl, $properties, false, $opts));
+if($show_label) :
+    echo ((int)$data == 0) ? $no_label : $yes_label ;
+else :
+    echo FabrikHelperHTML::image($icon, 'details', $tmpl, $properties, false, $opts);
+endif; 
