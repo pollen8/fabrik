0e4ffcc712b151015880f5b7c314a651c18a73c5
Jean-François Questiaux <info@betterliving.be>
2017-10-13 11:01:54 +0200

    Added aria-label on Fabrik buttons

--- components/com_fabrik/layouts/fabrik-button.php
+++ components/com_fabrik/layouts/fabrik-button.php
@@ -10,11 +10,12 @@ $attributes = isset($d->attributes) ? $d->attributes : '';
 $type       = isset($d->type) ? 'type="' . $d->type . '"' : 'type="button"';
 $tag        = isset($d->tag) ? $d->tag : 'button'; // button or a
 $name       = isset($d->name) ? 'name="' . $d->name . '"' : '';
-$id = isset($d->id) ? 'id="' . $d->id .'"' : '';
+$aria       = isset($d->aria) ? $d->aria : '';
+$id 	    = isset($d->id) ? 'id="' . $d->id .'"' : '';
 ?>
 
 <<?php echo $tag; ?> <?php echo $type; ?> class="btn <?php echo $d->class; ?>" <?php echo $attributes; ?>
-	<?php echo $name; ?> <?php echo $id; ?>>
+	<?php echo $name; ?> <?php echo $id; ?> <?php echo $aria; ?>>
 	<?php echo $d->label; ?>
 </<?php echo $tag; ?>>
 
