3abe5ce7423b65739091e48aa8fbaf73405598dc
Hugh Messenger <hugh.messenger@gmail.com>
2017-10-30 11:43:51 -0500

    Adding row-fluid:before and :after pseudo selector classes to bootstarp details template CSS, as they are missing from some Bootstrap templates (like JoomlArt/T3)

---
+++ components/com_fabrik/views/details/tmpl/bootstrap/template_css.php
@@ -0,0 +1,24 @@
+header('Content-type: text/css');
+$c = (int) $_REQUEST['c'];
+$view = isset($_REQUEST['view']) ? $_REQUEST['view'] : 'form';
+$rowid = isset($_REQUEST['rowid']) ? $_REQUEST['rowid'] : '';
+$form = $view . '_' . $c;
+
+if ($rowid !== '')
+{
+	$form .= '_' . $rowid;
+}
+
+echo <<<EOT
+# missing from some bootstrap templates (like JoomlArt)
+.row-fluid:before,
+.row-fluid:after {
+	display: table;
+	content: "";
+	line-height: 0;
+}
+
+.row-fluid:after {
+	clear: both;
+}
+EOT;
