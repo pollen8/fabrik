6e6d681095343b14e601cf1026740d649b3a8438
Hugh Messenger <hugh.messenger@gmail.com>
2017-12-06 15:15:12 -0600

    Added request input check for group start_collapsed

--- components/com_fabrik/views/list/view.base.php
+++ components/com_fabrik/views/list/view.base.php
@@ -288,7 +288,7 @@ class FabrikViewListBase extends FabrikView
 		$opts->groupByOpts                 = new stdClass;
 		$opts->groupByOpts->isGrouped      = (bool) $this->isGrouped;
 		$opts->groupByOpts->collapseOthers = (bool) $params->get('group_by_collapse_others', false);
-		$opts->groupByOpts->startCollapsed = (bool) $params->get('group_by_start_collapsed', false);
+		$opts->groupByOpts->startCollapsed = (bool) $input->get('start_collapsed', $params->get('group_by_start_collapsed', false));
 		$opts->groupByOpts->bootstrap      = FabrikWorker::j3();
 
 		// If table data starts as empty then we need the html from the row
