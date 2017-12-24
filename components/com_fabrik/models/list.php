722d52bb3e582422a70fcbf63ae14911a5bca291
Sophist <github@sodalis.co.uk>
2017-12-24 23:05:26 +0000

    Fix empty dropdown when php event list plugin used

--- components/com_fabrik/models/list.php
+++ components/com_fabrik/models/list.php
@@ -7111,7 +7111,7 @@ class FabrikFEModelList extends JModelForm
 			}
 
 			$pluginManager->runPlugins('button', $this, 'list', array('heading' => true));
-			$res = $pluginManager->data;
+			$res = array_filter($pluginManager->data);
 
 			if (FabrikWorker::j3())
 			{
