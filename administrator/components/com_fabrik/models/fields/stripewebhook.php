43dbbddc4a39c51f69497bca447c722382d39db2
Hugh Messenger <hugh.messenger@gmail.com>
2018-01-28 04:09:33 -0600

    Added stripewebhook field type

---
+++ administrator/components/com_fabrik/models/fields/stripewebhook.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Renders a Fabrik Help link
+ *
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ * @since       3.0.9
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Renders a Fabrik Help link
+ *
+ * @package  Fabrik
+ * @since    3.0.9
+ */
+
+class JFormFieldStripewebhook extends JFormField
+{
+	/**
+	 * Element name
+	 *
+	 * @access	protected
+	 * @var		string
+	 */
+	protected $name = 'Stripewebhook';
+
+	/**
+	 * Get the input - a read only link
+	 *
+	 * @return string
+	 */
+
+	public function getInput()
+	{
+		$formId = $this->form->model->getId();
+
+		if (empty($formId))
+		{
+			$url = FText::_('Available once form saved');
+		}
+		else
+		{
+			$url = COM_FABRIK_LIVESITE . 'index.php?option=com_fabrik&c=plugin&task=plugin.pluginAjax';
+			$url .= '&formid=' . $formId;
+			$url .= '&g=form&plugin=stripe&method=webhook';
+			$url .= '&renderOrder=' . $this->form->repeatCounter;
+		}
+
+		return '<div>' . $url . '</div>';
+	}
+}
