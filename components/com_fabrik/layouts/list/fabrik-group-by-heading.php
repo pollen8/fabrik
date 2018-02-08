999b47874ef0e8fae3da19f15f7f0c78d3b1a8c8
Hugh Messenger <hugh.messenger@gmail.com>
2018-02-08 12:34:33 -0600

    Tweak to group-by heading template, don't include actual heading in the collapse toggle link, in case heading has a link.

--- components/com_fabrik/layouts/list/fabrik-group-by-heading.php
+++ components/com_fabrik/layouts/list/fabrik-group-by-heading.php
@@ -18,15 +18,15 @@ $imgProps = array('alt' => FText::_('COM_FABRIK_TOGGLE'), 'data-role' => 'toggle
 
 <?php if ($d->emptyDataMessage != '') : ?>
 <a href="#" class="toggle">
-	<?php else: ?>
-	<a href="#" class="toggle fabrikTip" title="<?php echo $d->emptyDataMessage ?>" opts='{"trigger": "hover"}'>
-		<?php endif; ?>
-		<?php echo FabrikHelperHTML::image('arrow-down', 'list', $d->tmpl, $imgProps); ?>
-		<span class="groupTitle">
-			<?php echo $d->title; ?> 
-			<?php $d->group_by_show_count = isset($d->group_by_show_count) ? $d->group_by_show_count : '1'; 
-			if ($d->group_by_show_count) : ?>
-				<span class="groupCount">(<?php echo $d->count ?>)</span>
-			<?php endif; ?>			
-		</span>
-	</a>
+<?php else: ?>
+<a href="#" class="toggle fabrikTip" title="<?php echo $d->emptyDataMessage ?>" opts='{"trigger": "hover"}'>
+<?php endif; ?>
+<?php echo FabrikHelperHTML::image('arrow-down', 'list', $d->tmpl, $imgProps); ?>
+</a>
+<span class="groupTitle">
+<?php echo $d->title; ?>
+<?php $d->group_by_show_count = isset($d->group_by_show_count) ? $d->group_by_show_count : '1';
+if ($d->group_by_show_count) : ?>
+<span class="groupCount">(<?php echo $d->count ?>)</span>
+<?php endif; ?>
+</span>
