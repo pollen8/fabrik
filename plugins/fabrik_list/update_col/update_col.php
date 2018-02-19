3d824f762b60fc7abe12dcf8eff172465b19a0b2
Hugh Messenger <hugh.messenger@gmail.com>
2018-02-19 17:23:56 -0600

    Run fullPaths() on update_col email, plus convert to new helper namespacing.

--- plugins/fabrik_list/update_col/update_col.php
+++ plugins/fabrik_list/update_col/update_col.php
@@ -11,6 +11,8 @@
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
+use Fabrik\Helpers\Pdf;
+use Fabrik\Helpers\Worker;
 use Joomla\Utilities\ArrayHelper;
 
 // Require the abstract plugin class
@@ -236,7 +238,7 @@ class PlgFabrik_ListUpdate_col extends PlgFabrik_List
 		if (!empty($preEval))
 		{
 			$res = FabrikHelperHTML::isDebug() ? eval($preEval) : @eval($preEval);
-			FabrikWorker::logEval($res, 'Caught exception on eval in updatecol::process() : %s');
+			Worker::logEval($res, 'Caught exception on eval in updatecol::process() : %s');
 
 			if ($res === false)
 			{
@@ -290,7 +292,7 @@ class PlgFabrik_ListUpdate_col extends PlgFabrik_List
 		{
 			@trigger_error('');
 			$msg = @eval($postEval);
-			FabrikWorker::logEval($msg, 'Caught exception on eval in updatecol::process() : %s');
+			Worker::logEval($msg, 'Caught exception on eval in updatecol::process() : %s');
 
 			if (!empty($msg))
 			{
@@ -324,8 +326,7 @@ class PlgFabrik_ListUpdate_col extends PlgFabrik_List
 
 		if (!empty($emailColID) || !empty($emailTo))
 		{
-			$w = new FabrikWorker;
-			jimport('joomla.mail.helper');
+			$w = new Worker;
 			$aids = explode(',', $ids);
 			$message = $params->get('update_email_msg');
 			$subject = $params->get('update_email_subject');
@@ -353,7 +354,7 @@ class PlgFabrik_ListUpdate_col extends PlgFabrik_List
 				{
 					$email = trim($email);
 
-					if (!(JMailHelper::cleanAddress($email) && FabrikWorker::isEmail($email)))
+					if (!(JMailHelper::cleanAddress($email) && Worker::isEmail($email)))
 					{
 						$cleanTo = false;
 					}
@@ -372,11 +373,20 @@ class PlgFabrik_ListUpdate_col extends PlgFabrik_List
 					if ($eval)
 					{
 						$thisMessage = @eval($thisMessage);
-						FabrikWorker::logEval($thisMessage, 'Caught exception on eval in updatecol::process() : %s');
+						Worker::logEval($thisMessage, 'Caught exception on eval in updatecol::process() : %s');
 					}
 
-					$mail = JFactory::getMailer();
-					$res = $mail->sendMail($from, $fromName, $to, $thisSubject, $thisMessage, true);
+					Pdf::fullPaths($thisMessage);
+
+					$res = Worker::sendMail(
+						$from,
+						$fromName,
+						$to,
+						$thisSubject,
+						$thisMessage,
+						true
+					);
+
 
 					if ($res)
 					{
@@ -465,7 +475,7 @@ class PlgFabrik_ListUpdate_col extends PlgFabrik_List
 		$params = $this->getParams();
 		$emailColID = $params->get('update_email_element', '');
 
-		return FabrikWorker::getPluginManager()->getElementPlugin($emailColID);
+		return Worker::getPluginManager()->getElementPlugin($emailColID);
 	}
 
 	/**
@@ -505,7 +515,7 @@ class PlgFabrik_ListUpdate_col extends PlgFabrik_List
             if ($params->get('update_email_to_eval', '0') === '1')
             {
                 $to = @eval($to);
-                FabrikWorker::logEval($to, 'Caught exception on eval in updatecol::emailTo() : %s');
+                Worker::logEval($to, 'Caught exception on eval in updatecol::emailTo() : %s');
             }
 		}
 
@@ -540,7 +550,7 @@ class PlgFabrik_ListUpdate_col extends PlgFabrik_List
 		if ($eval)
 		{
 			$val = @eval($val);
-			FabrikWorker::logEval($val, 'Caught exception on eval in updatecol::_process() : %s');
+			Worker::logEval($val, 'Caught exception on eval in updatecol::_process() : %s');
 		}
 
 		$model->updateRows($ids, $col, $val);
@@ -607,16 +617,15 @@ class PlgFabrik_ListUpdate_col extends PlgFabrik_List
 		$listRef = $model->getRenderContext();
 		$prefix = 'fabrik___update_col[list_' . $listRef . '][';
 		$elements = '<select class="inputbox key update_col_elements" size="1" name="' . $prefix . 'key][]">' . implode("\n", $options) . '</select>';
-		$j3 = FabrikWorker::j3();
-		$addImg = $j3 ? 'plus' : 'add.png';
-		$removeImg = $j3 ? 'remove' : 'del.png';
+		$addImg = 'plus';
+		$removeImg = 'remove';
 
 
 		$layout = $this->getLayout('form');
 		$layoutData = new stdClass;
 		$layoutData->listRef = $listRef;
 		$layoutData->renderOrder = $this->renderOrder;
-		$layoutData->j3 = $j3;
+		$layoutData->j3 = true;
 		$layoutData->addImg = FabrikHelperHTML::image($addImg, 'list', $model->getTmpl());
 		$layoutData->delImg = FabrikHelperHTML::image($removeImg, 'list', $model->getTmpl());
 		$layoutData->elements = $elements;
