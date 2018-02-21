273a3d33b847d920a996f2918c37040d167614a4
Hugh Messenger <hugh.messenger@gmail.com>
2018-02-21 11:27:33 -0600

    Only show table name in map viz filters if more than one list is used.

--- plugins/fabrik_visualization/googlemap/views/googlemap/tmpl/bootstrap/default_filter.php
+++ plugins/fabrik_visualization/googlemap/views/googlemap/tmpl/bootstrap/default_filter.php
@@ -12,55 +12,64 @@
 defined('_JEXEC') or die('Restricted access');
 
 if ($this->showFilters) :
-?>
-<form method="post" name="filter" action="<?php echo $this->filterFormURL; ?>">
-<?php
-	foreach ($this->filters as $table => $filters) :
-		if (!empty($filters)) :
-		?>
-	  <table class="filtertable table table-striped">
-
-	   <thead>
-	  	<tr>
-	  		<th><?php echo $table ?></th>
-	  		<th style="text-align:right">
-	  			<a href="#" class="clearFilters">
-				    <?php echo FabrikHelperHTML::icon('icon-refresh'); ?> <?php echo FText::_('COM_FABRIK_CLEAR'); ?>
-	  			</a>
-	  		</th>
-	  	</tr>
-	  </thead>
-
-	  <tfoot>
-	  	<tr>
-	  		<th colspan="2" style="text-align:right;">
-	  			<button type="submit" class="btn btn-primary">
-				    <?php echo FabrikHelperHTML::icon('icon-filter'); ?> <?php echo FText::_('COM_FABRIK_GO') ?>
-	  			</button>
-	  		</th>
-	  	</tr>
-	  </tfoot>
-
-	  <tbody>
-	  <?php
-			$c = 0;
-			foreach ($filters as $filter) :
-				$required = $filter->required == 1 ? ' class="notempty"' : '';
-			 ?>
-	    <tr class="fabrik_row oddRow<?php echo ($c % 2); ?>">
-	    	<td<?php echo $required ?>><?php echo $filter->label ?> </td>
-	    	<td><?php echo $filter->element ?></td>
-	    </tr>
-	  <?php
-				$c++;
-			endforeach;
-			?>
-	  </tbody>
-	  </table>
-	  <?php
-		endif;
-	endforeach;
 	?>
-</form>
+    <form method="post" name="filter" action="<?php echo $this->filterFormURL; ?>">
+		<?php
+		foreach ($this->filters as $table => $filters) :
+			if (!empty($filters)) :
+				?>
+                <table class="filtertable table table-striped">
+                    <thead>
+                    <tr>
+						<?php
+						if (count($this->filters) > 1) :
+						?>
+                        <th><?php echo $table ?></th>
+                        <th style="text-align:right">
+                            <?php
+							else :
+							?>
+                        <th colspan="2" style="text-align:right">
+							<?php
+							endif;
+							?>
+                            <a href="#" class="clearFilters">
+								<?php echo FabrikHelperHTML::icon('icon-refresh'); ?><?php echo FText::_('COM_FABRIK_CLEAR'); ?>
+                            </a>
+                        </th>
+                    </tr>
+                    </thead>
+
+                    <tfoot>
+                    <tr>
+                        <th colspan="2" style="text-align:right;">
+                            <button type="submit" class="btn btn-primary">
+								<?php echo FabrikHelperHTML::icon('icon-filter'); ?><?php echo FText::_('COM_FABRIK_GO') ?>
+                            </button>
+                        </th>
+                    </tr>
+                    </tfoot>
+
+                    <tbody>
+					<?php
+					$c = 0;
+					foreach ($filters as $filter) :
+						$required = $filter->required == 1 ? ' class="notempty"' : '';
+						?>
+                        <tr class="fabrik_row oddRow<?php echo($c % 2); ?>">
+                            <td<?php echo $required ?>><?php echo $filter->label ?> </td>
+                            <td><?php echo $filter->element ?></td>
+                        </tr>
+						<?php
+						$c++;
+					endforeach;
+					?>
+                    </tbody>
+                </table>
+			<?php
+			endif;
+		endforeach;
+		?>
+    </form>
 <?php
 endif;
