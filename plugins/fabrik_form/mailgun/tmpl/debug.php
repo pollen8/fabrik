8b11542ec54e9da832d98585c7fe179b01ae5bc6
Hugh Messenger <hugh.messenger@gmail.com>
2018-02-23 20:27:02 -0600

    First cut at Mailgun plugin, supporting inbound mail.  Initially is a copy of email plugin, with added functionality.  Will probably split it out, so mailgun plugin is only for inbound, and add Mailgun API option for sending in main email plugin.

---
+++ plugins/fabrik_form/mailgun/tmpl/debug.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This is a sample email template. It will just print out all of the request data:
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Fabrik.form.email
+ * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+?>
+<table border="1">
+<?php
+foreach ($this->data as $key => $val)
+{
+
+	echo '<tr><td>' . $key . '</td><td>';
+	if (is_array($val)) :
+		foreach ($val as $v):
+			if (is_array($v)) :
+				echo implode("<br>", $v);
+			else:
+				echo implode("<br>", $val);
+			endif;
+		endforeach;
+	else:
+		echo $val;
+	endif;
+	echo "</td></tr>";
+}
+?>
+</table>
