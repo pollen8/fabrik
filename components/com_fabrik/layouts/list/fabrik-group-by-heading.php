24a5e8777b312b49d3d8db1da51eaf029d1d9d7b
Hugh Messenger <hugh.messenger@gmail.com>
2018-03-12 08:11:57 -0500

    Strip tags from group title, moved closing tag to end again

--- components/com_fabrik/layouts/list/fabrik-group-by-heading.php
+++ components/com_fabrik/layouts/list/fabrik-group-by-heading.php
@@ -22,11 +22,11 @@ $imgProps = array('alt' => FText::_('COM_FABRIK_TOGGLE'), 'data-role' => 'toggle
 <a href="#" class="toggle fabrikTip" title="<?php echo $d->emptyDataMessage ?>" opts='{"trigger": "hover"}'>
 <?php endif; ?>
 <?php echo FabrikHelperHTML::image('arrow-down', 'list', $d->tmpl, $imgProps); ?>
-</a>
 <span class="groupTitle">
-<?php echo $d->title; ?>
+<?php echo strip_tags($d->title); ?>
 <?php $d->group_by_show_count = isset($d->group_by_show_count) ? $d->group_by_show_count : '1';
 if ($d->group_by_show_count) : ?>
 <span class="groupCount">(<?php echo $d->count ?>)</span>
 <?php endif; ?>
 </span>
+</a>
