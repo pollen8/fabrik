cd44bb5272ec443c575e565f8b9383ca5dbdc079
Hugh Messenger <hugh.messenger@gmail.com>
2018-03-25 13:38:47 -0500

    Attempt at handling repeat group data in isuniquevalue validation.  Also added "within this group" option, where it only checks for uniqueness within the repeats for this parent.  Haven't done a huge amount of testing (haven't tested non-repeat joins), but basic use cases seem to work.

--- plugins/fabrik_validationrule/isuniquevalue/isuniquevalue.php
+++ plugins/fabrik_validationrule/isuniquevalue/isuniquevalue.php
@@ -11,6 +11,8 @@
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
+use Joomla\Utilities\ArrayHelper;
+
 // Require the abstract plugin class
 require_once COM_FABRIK_FRONTEND . '/models/validation_rule.php';
 
@@ -54,8 +56,6 @@ class PlgFabrik_ValidationruleIsUniqueValue extends PlgFabrik_Validationrule
 		$listModel   = $elementModel->getlistModel();
 		$table       = $listModel->getTable();
 		$db          = $listModel->getDb();
-		//$lookupTable = $db->qn($table->db_table_name);
-		$data        = $db->q($data);
 		$query       = $db->getQuery(true);
 		$cond        = $params->get('isuniquevalue-caseinsensitive') == 1 ? 'LIKE' : '=';
 		$secret      = $this->config->get('secret');
@@ -65,6 +65,15 @@ class PlgFabrik_ValidationruleIsUniqueValue extends PlgFabrik_Validationrule
 		// if it's a join, get the joined table name
 		if ($groupModel->isJoin())
 		{
+			if ($groupModel->canRepeat() && $params->get('isuniquevalue-within-group', '0') === '1')
+			{
+				/*
+				 * If "within group" option selected, we don't care about the state of the database, we only care
+				 * whether the submitted data for this repeat group has a dupe
+				 */
+				return $this->checkThisGroup($data, $repeatCounter);
+			}
+
 			$lookupTable = $groupModel->getJoinModel()->getJoin()->table_join;
 		}
 		else
@@ -81,14 +90,11 @@ class PlgFabrik_ValidationruleIsUniqueValue extends PlgFabrik_Validationrule
 			$k = $db->qn($element->name);
 		}
 
-		$query->select('COUNT(*)')->from($db->qn($lookupTable))->where($k . ' ' . $cond . ' ' . $data);
+		$query->select('COUNT(*)')->from($db->qn($lookupTable))->where($k . ' ' . $cond . ' ' . $db->q($data));
 
 		/*
-		 * $$$ hugh - need to check to see if we're editing a record, so we can exclude this record
-		 *
-		 * Need to figure out if this is a joined element, and set PK and 'rowid' accordingly
-		 *
-		 * NOTE - probably only works for non-repeat joins
+		 * Check to see if we're editing a record, so we can exclude this record, and handle joined / repeated
+		 * data accordingly.
 		 */
 
 		if (!$groupModel->isJoin())
@@ -100,9 +106,29 @@ class PlgFabrik_ValidationruleIsUniqueValue extends PlgFabrik_Validationrule
 		else
 		{
 			// join, so get the join's PK and value as rowid
-			$pk = $groupModel->getJoinModel()->getForeignID();
-			$rowId = $this->formModel->formData[$pk];
-			$pk = $groupModel->getJoinModel()->getForeignID('.');
+			$joinModel = $groupModel->getJoinModel();
+			$pk = $joinModel->getForeignID();
+
+			if ($groupModel->canRepeat())
+			{
+				/**
+				 * Check if there's a dupe in this repeat, which might not show in a database lookup, if they've
+				 * added repeats, or changed existing repeats.  if so, don't even bother doing the lookup, just bail.
+				 */
+				if (!$this->checkThisGroup($data, $repeatCounter))
+				{
+					return false;
+				}
+
+				$rowids = Arrayhelper::getValue($this->formModel->formData, $pk, array());
+				$rowId = ArrayHelper::getValue($rowids, $repeatCounter, '');
+			}
+			else
+			{
+				$rowId = Arrayhelper::getValue($this->formModel->formData, $pk, '');
+			}
+
+			$pk = $joinModel->getForeignID('.');
 		}
 
 		if (!empty($rowId))
@@ -114,5 +140,46 @@ class PlgFabrik_ValidationruleIsUniqueValue extends PlgFabrik_Validationrule
 		$c = $db->loadResult();
 
 		return ($c === '0') ? true : false;
+	}
+
+	/**
+	 * Check for duplicate values with the submitted data for this repeat group
+	 *
+	 * @param   string $data          To check
+	 * @param   int    $repeatCounter Repeat group counter
+	 *
+	 * @return  bool  true if validation passes, false if fails
+	 */
+	private function checkThisGroup($data, $repeatCounter)
+	{
+		$params = $this->getParams();
+		$elementModel = $this->elementModel;
+		$groupModel = $elementModel->getGroup();
+
+		if ($groupModel->canRepeat())
+		{
+			$fullName = $elementModel->getFullName(true, false);
+			$elementData = ArrayHelper::getValue($this->formModel->formData, $fullName, array());
+
+			foreach ($elementData as $k => $v)
+			{
+				if ($k === (int)$repeatCounter)
+				{
+					continue;
+				}
+
+				if (is_array($v))
+				{
+					$v = implode('', $v);
+				}
+
+				if ($v === $data)
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
 	}
 }
