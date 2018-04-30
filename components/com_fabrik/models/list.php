02e3750c18fccd1c321bfcc13d3e45d56f7910d5
Sophist <github@sodalis.co.uk>
2018-04-30 11:05:53 +0100

    Do not show default repeat group elements in list

--- components/com_fabrik/models/list.php
+++ components/com_fabrik/models/list.php
@@ -8265,6 +8265,9 @@ class FabrikFEModelList extends JModelForm
 		$item->name = $item->label = 'id';
 		$item->group_id = $groupId;
 
+		// PR#xxxx do not show repeat group parent_id in list by default
+		$item->show_in_list_summary = 0;
+
 		if (!$item->store())
 		{
 			JError::raiseWarning(500, $item->getError());
@@ -8292,6 +8295,9 @@ class FabrikFEModelList extends JModelForm
 		$item->name = $item->label = 'parent_id';
 		$item->hidden = 1;
 		$item->group_id = $groupId;
+
+		// PR#xxxx do not show repeat group parent_id in list by default
+		$item->show_in_list_summary = 0;
 
 		if (!$item->store())
 		{
