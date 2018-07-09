b50cfc683f5abc7b69203ca07f14e00aa3552af1
Hugh Messenger <hugh.messenger@gmail.com>
2018-07-09 16:06:17 -0500

    Fix for dropdown / radio types in isgreaterorlessthan validation

--- plugins/fabrik_validationrule/isgreaterorlessthan/isgreaterorlessthan.php
+++ plugins/fabrik_validationrule/isgreaterorlessthan/isgreaterorlessthan.php
@@ -79,6 +79,7 @@ class PlgFabrik_ValidationruleIsgreaterorlessthan extends PlgFabrik_Validationru
 
 		$otherElementModel = $this->getOtherElement();
 		$compare = $compareValue === '' ? $otherElementModel->getValue($formData, $repeatCounter) : $compareValue;
+		$compare = is_array($compare) ? array_pop($compare) : $compare;
 
 		if ($this->allowEmpty() && ($data === '' || $compare === ''))
 		{
