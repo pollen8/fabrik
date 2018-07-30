46169e9f04a92ed1d59e56b956128d66199a31f1
Hugh Messenger <hugh.messenger@gmail.com>
2018-07-30 13:01:02 -0500

    Increase font size of lock icons in lockrow plugin

--- plugins/fabrik_element/lockrow/layouts/fabrik-element-lockrow-list.php
+++ plugins/fabrik_element/lockrow/layouts/fabrik-element-lockrow-list.php
@@ -6,6 +6,6 @@ $d = $displayData;
 ?>
 <div class="<?php echo $d->class; ?>">
 	<?php
-		echo FabrikHelperHTML::image($d->icon, 'list', $d->tmpl, array('title' => $d->alt));
+		echo FabrikHelperHTML::image($d->icon, 'list', $d->tmpl, array('title' => $d->alt, 'style' => 'font-size:18px'));
 	?>
 </div>
