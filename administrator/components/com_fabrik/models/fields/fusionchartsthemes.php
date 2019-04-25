ac15f650f4ecb5e1c844c728ee8f48c1d538d79f
Hugh Messenger <hugh.messenger@gmail.com>
2019-04-25 15:00:53 -0500

    Added new 'fusionchartsthemes' JFormField type, to support picking the theme to use for a chart from the FC library themes folder (needed custo field because the FC lib folder is also chosen as a param, so not a fixed location).

---
+++ administrator/components/com_fabrik/models/fields/fusionchartsthemes.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Renders a list of connections
+ *
+ * @package     Joomla
+ * @subpackage  Form
+ * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+require_once JPATH_ADMINISTRATOR . '/components/com_fabrik/helpers/element.php';
+
+jimport('joomla.html.html');
+jimport('joomla.form.formfield');
+jimport('joomla.form.helper');
+JFormHelper::loadFieldClass('filelist');
+
+/**
+ * Renders a list of connections
+ *
+ * @package     Joomla
+ * @subpackage  Form
+ * @since       3.0
+ */
+class JFormFieldFusionchartsthemes extends JFormFieldFileList
+{
+	/**
+	 * Element name
+	 *
+	 * @var        string
+	 */
+	protected $name = 'Fusionchartsthemes';
+
+	/**
+	 * Method to get the field options.
+	 *
+	 * @return  array  The field option objects.
+	 */
+	protected function getOptions()
+	{
+		$params = $this->form->getData()->get('params');
+		$fcLib = $params->fusionchart_library;
+
+		$this->directory = JPATH_ROOT . '/plugins/fabrik_visualization/fusionchart/libs/' . $fcLib . '/js/themes';
+		$this->hideDefault = true;
+
+		$opts = parent::getOptions();
+
+		foreach ($opts as &$opt)
+		{
+			$matches = array();
+			if (preg_match('/fusioncharts\.theme\.(\w+)\.js/', $opt->value, $matches))
+			{
+				$opt->value = $matches[1];
+				$opt->text = $matches[1];
+			}
+		}
+
+		return $opts;
+	}
+}
