bdaa03382c04f332d50c2dd226e577242da5ee80
Hugh Messenger <hugh.messenger@gmail.com>
2019-04-29 14:35:26 -0500

    Fix for Paypal return URL using https

--- plugins/fabrik_form/paypal/paypal.php
+++ plugins/fabrik_form/paypal/paypal.php
@@ -371,7 +371,7 @@ class PlgFabrik_FormPaypal extends PlgFabrik_Form
 
         if ($testMode == 1 && !empty($paypal_return_url))
         {
-            if (preg_match('#^http:\/\/#', $paypal_return_url))
+            if (preg_match('#^(http|https):\/\/#', $paypal_return_url))
             {
                 $opts['return'] = $paypal_return_url;
             }
@@ -394,7 +394,7 @@ class PlgFabrik_FormPaypal extends PlgFabrik_Form
         }
         elseif (!empty($paypal_return_url))
         {
-            if (preg_match('#^http:\/\/#', $paypal_return_url))
+            if (preg_match('#^(http|https):\/\/#', $paypal_return_url))
             {
                 $opts['return'] = $paypal_return_url;
             }
