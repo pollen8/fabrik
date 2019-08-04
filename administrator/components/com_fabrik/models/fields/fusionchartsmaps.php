18ecf99c4eff77daa8661cea0f843d237adb06fc
Hugh Messenger <hugh.messenger@gmail.com>
2019-08-04 01:31:40 -0500

    Added new JField type for 'fusionchartsmaps', to list content of maps folder of selected version of FC

---
+++ administrator/components/com_fabrik/models/fields/fusionchartsmaps.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Renders a list of connections
+ *
+ * @package     Joomla
+ * @subpackage  Form
+ * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+require_once JPATH_ADMINISTRATOR . '/components/com_fabrik/helpers/element.php';
+
+jimport('joomla.html.html');
+jimport('joomla.form.formfield');
+jimport('joomla.form.helper');
+JFormHelper::loadFieldClass('filelist');
+
+/**
+ * Renders a list of connections
+ *
+ * @package     Joomla
+ * @subpackage  Form
+ * @since       3.0
+ */
+class JFormFieldFusionchartsmaps extends JFormFieldFileList
+{
+	/**
+	 * Element name
+	 *
+	 * @var        string
+	 */
+	protected $name = 'Fusionchartsmaps';
+
+	/**
+	 * Method to get the field options.
+	 *
+	 * @return  array  The field option objects.
+	 */
+	protected function getOptions()
+	{
+		$params = $this->form->getData()->get('params');
+
+		$opts = array();
+
+		if ($params)
+		{
+			$fcLib = $params->fusionchart_library;
+
+			if (!empty($fcLib))
+			{
+				$this->directory   = JPATH_ROOT . '/plugins/fabrik_visualization/fusionchart/libs/' . $fcLib . '/js/maps';
+				$this->hideDefault = true;
+
+				$opts = parent::getOptions();
+
+				foreach ($opts as &$opt)
+				{
+					$matches = array();
+					if (preg_match('/fusioncharts\.(\w+)\.js/', $opt->value, $matches))
+					{
+						$opt->value = $matches[1];
+						$opt->text  = $matches[1];
+					}
+				}
+			}
+		}
+
+		return $opts;
+	}
+}
