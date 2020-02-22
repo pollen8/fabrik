2885065ee36a59ab5eaf9e22079c5b245cedc0ae
Hugh Messenger <hugh.messenger@gmail.com>
2020-02-22 14:51:46 -0600

    Add support for Twitch and Streamable to youtube element

--- plugins/fabrik_element/youtube/layouts/fabrik-element-youtube-detail.php
+++ plugins/fabrik_element/youtube/layouts/fabrik-element-youtube-detail.php
@@ -6,6 +6,7 @@ $d      = $displayData;
 $uri    = JUri::getInstance();
 $scheme = $uri->getScheme();
 
+if ($d->type === 'youtube') :
 ?>
 
 <iframe
@@ -13,6 +14,20 @@ $scheme = $uri->getScheme();
 	type="text/html"
 	width="<?php echo $d->width;?>"
 	height="<?php echo $d->height; ?>"
-    src="<?php echo $scheme; ?>://www.youtube.com/embed/<?php echo $d->vid; ?>?autoplay=<?php echo $d->autoplay; ?>&fs=<?php echo $d->fs; ?>"
+    src="<?php echo $d->url; ?><?php echo $d->vid; ?>?autoplay=<?php echo $d->autoplay; ?>&fs=<?php echo $d->fs; ?>"
     frameborder="0"
 ></iframe>
+<?php
+elseif ($d->type === 'twitch') :
+?>
+	<iframe
+		src="<?php echo $d->url; ?><?php echo $d->vid; ?>"
+		width="<?php echo $d->width;?>"
+		height="<?php echo $d->height; ?>"
+		frameborder="0"
+		scrolling="no"
+		allowfullscreen="true">
+	</iframe>
+<?php
+endif;
+?>
