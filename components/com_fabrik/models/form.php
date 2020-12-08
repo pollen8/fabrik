e6c5709d448984cf4bcc574a65a3bf53fe2f830b
troester <trob@users.noreply.github.com>
2020-12-08 14:20:26 +0100

    form.php: remove get_magic_quotes_gpc
    (always false since php5.4, deprecated in php7.4)

--- components/com_fabrik/models/form.php
+++ components/com_fabrik/models/form.php
@@ -2314,7 +2314,8 @@ class FabrikFEModelForm extends FabModelForm
 
 					// $$$ rob $this->formData was $_POST, but failed to get anything for calculation elements in php 5.2.1
 					$formData = $elementModel->getValue($this->formData, $c, array('runplugins' => 0, 'use_default' => false, 'use_querystring' => false));
-
+					
+					/* remove get_magic_quotes_gpc (always false since php5.4, deprecated in php7.4)
 					if (get_magic_quotes_gpc())
 					{
 						if (is_array($formData))
@@ -2342,6 +2343,7 @@ class FabrikFEModelForm extends FabModelForm
 							}
 						}
 					}
+					*/
 
 					// Internal element plugin validations
 					if (!$elementModel->validate(@$formData, $c))
