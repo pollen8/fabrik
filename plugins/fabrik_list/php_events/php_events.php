20f0719de6ec4a27392c8eb65c75bf1d179f93b7
Hugh Messenger <hugh.messenger@gmail.com>
2021-06-17 02:10:31 -0500

    Added new onElementUseAsRowClass plugin, can add classes to list row class on per element basis.

--- plugins/fabrik_list/php_events/php_events.php
+++ plugins/fabrik_list/php_events/php_events.php
@@ -239,6 +239,13 @@ class PlgFabrik_ListPhp_Events extends PlgFabrik_List
 		return $this->doEvaluate($params->get('list_phpevents_onelementcanviewlist'), $args);
 	}
 
+	public function onElementSetRowClass(&$args)
+	{
+		$params = $this->getParams();
+
+		return $this->doEvaluate($params->get('list_phpevents_onelementsetrowclass'), $args);
+	}
+
 	public function onRelatedDataURL(&$args)
 	{
 		$params = $this->getParams();
