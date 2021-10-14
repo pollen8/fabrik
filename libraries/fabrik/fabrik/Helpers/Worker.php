fa3bbef4c6fb92f3652ce6debb5d5070ee6472f2
Alex Chartier <achartier@admiralsecure.com>
2021-10-14 14:35:36 -0400

    Check that return is not a boolean before checking its parent class

--- libraries/fabrik/fabrik/Helpers/Worker.php
+++ libraries/fabrik/fabrik/Helpers/Worker.php
@@ -2397,7 +2397,7 @@ class Worker
 		}
 
         // if ACY mailing is installed, it returns an exception (!) rather than false
-        if (get_parent_class($ret) === 'Exception')
+        if (is_bool($ret) === false && get_parent_class($ret) === 'Exception')
         {
             self::log('fabrik.helper.sendmail.error', 'Exception in Send: ' . $ret->getMessage(), false);
 
